6bb2090028a8e9eecd7a5f6638d2e516fcaf1b39
hamilton <hamilton.ulmer@gmail.com>
2017-10-05 14:20:31 -0700

    baby's first test for cells

--- __tests__/cell-reducer.test.js
+++ __tests__/cell-reducer.test.js
@@ -1,5 +1,18 @@
 import cell from './../src/reducers/cell-reducer'
-import * as utils from './../src/notebook-utils'
+import * as NB from './../src/notebook-utils'
 import actions                 from './../src/actions'
 
 
+describe('add cells', ()=>{
+    var state = NB.newNotebook()
+    var nextState = cell(state, {type: 'ADD_CELL', cellType: 'javascript'})
+    it('should add a cell to the end of the current notebook', ()=> {
+        expect(nextState.cells.length).toEqual(NB.newNotebook().cells.length+1)
+        expect(nextState.cells[nextState.cells.length-1].cellType).toEqual('javascript')
+    })
+})
+
+// describe('insert cells', ()=> {
+//     var state = NB.newNotebook()
+//     var nextState = cell()
+// })
