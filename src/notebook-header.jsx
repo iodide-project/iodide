27a7b2a88e37812775dd09c1cd72c03435452c5c
bcolloran <bcolloran@mozilla.com>
2017-10-30 20:14:09 -0700

    code clean up (deleted commented out stuff, tabs to spaces, removed newlines)

--- src/notebook-header.jsx
+++ src/notebook-header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import { Button, ButtonToolbar, ToggleButtonGroup, 
-// 	ToggleButton, Label, DropdownButton, MenuItem, Dropdown,
-// 	Navbar, Nav, NavItem, Glyphicon } from 'react-bootstrap'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import settings from './settings.jsx'
 
 import {HistoryCell} from './cell.jsx'
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar'
 import {red500, yellow500, blue500, grey900,grey50} from 'material-ui/styles/colors'
-
 
 import FlatButton from 'material-ui/FlatButton'
 import IconButton from 'material-ui/IconButton'
@@ -37,198 +37,177 @@ import {prettyDate, formatDateString} from './notebook-utils'
 
 import Title from './title.jsx'
 
-
 // TODO: replace settings w/ a settings file that we can share everywhere.
-
 
 const AUTOSAVE = settings.labels.AUTOSAVE
 
-
 function stateIsValid(state) {
-	// TODO - fill this out and figure out if everything is in order.
-	return state !== undefined // for now ...
+  // TODO - fill this out and figure out if everything is in order.
+  return state !== undefined // for now ...
 }
 
 class NotebookHeader extends React.Component {
-	constructor(props) {
-		super(props)
-		this.notebookFileImport = this.notebookFileImport.bind(this)
-		this.state = {previousMode: props.mode}
-	}
+  constructor(props) {
+    super(props)
+    this.notebookFileImport = this.notebookFileImport.bind(this)
+    this.state = {previousMode: props.mode}
+  }
 
-	notebookFileImport(evt) {
-		var fn = evt.target.files[0]
-		var reader = new FileReader()
-		reader.onload = (result) => {
-			var newState
-			try {
-				newState = JSON.parse(result.target.result)	
-			} catch(e) {
-				console.error(e)
-			}
-			
-			if (stateIsValid(newState)) {
-				this.props.actions.importNotebook(newState)
-			}
-			
-		}
-		reader.readAsText(fn)
-	}
+  notebookFileImport(evt) {
+    var fn = evt.target.files[0]
+    var reader = new FileReader()
+    reader.onload = (result) => {
+      var newState
+      try {
+        newState = JSON.parse(result.target.result)  
+      } catch(e) {
+        console.error(e)
+      }
+      if (stateIsValid(newState)) {
+        this.props.actions.importNotebook(newState)
+      }
+    }
+    reader.readAsText(fn)
+  }
 
-	render() {
-		var histContents = []
-		if (this.props.history.length) {
-		histContents = this.props.history.map((cell,i)=> {
-			var cellComponent = <HistoryCell display={true} ref={'cell'+cell.id} actions={this.props.actions} cell={cell} id={i+'-'+cell.id} key={'history'+i} />
-			return cellComponent
-		})
-		} else {
-			histContents.push(<div className='no-history'>No History</div>)
-		}
+  render() {
+    var histContents = []
+    if (this.props.history.length) {
+    histContents = this.props.history.map((cell,i)=> {
+      var cellComponent = <HistoryCell display={true} ref={'cell'+cell.id} actions={this.props.actions} cell={cell} id={i+'-'+cell.id} key={'history'+i} />
+      return cellComponent
+    })
+    } else {
+      histContents.push(<div className='no-history'>No History</div>)
+    }
 
-		return (
-			<div className='notebook-header'>
-				<input id='import-notebook' 
-							name='file'
-							type='file' style={{display:'none'}} onChange={this.notebookFileImport} 
-						/>
-				<a id='export-anchor' style={{display: 'none'}} ></a>
-				<div className='notebook-menu' style={{display: this.props.viewMode === 'editor' ? 'block' : 'none'}}>
-						<EditorMenu actions={this.props.actions}
-							mode={this.props.mode}
-							cells={this.props.cells}
-							viewMode={this.props.viewMode}
-							title={this.props.title}
-							sidePaneMode={this.props.sidePaneMode}
-							lastSaved={this.props.lastSaved} />
-				</div>
-				<div className='presentation-menu' style={{display: (this.props.viewMode === 'presentation' ? 'block' : 'none')}} >
-					<PresentationMenu
-						mode={this.props.mode}
-						cells={this.props.cells}
-						viewMode={this.props.viewMode}
-						title={this.props.title}
-						actions={this.props.actions}
-						sidePaneMode={this.props.sidePaneMode}
-						lastSaved={this.props.lastSaved} />
-				</div>
+    return (
+      <div className='notebook-header'>
+        <input id='import-notebook' 
+              name='file'
+              type='file' style={{display:'none'}} onChange={this.notebookFileImport} 
+            />
+        <a id='export-anchor' style={{display: 'none'}} ></a>
+        <div className='notebook-menu' style={{display: this.props.viewMode === 'editor' ? 'block' : 'none'}}>
+            <EditorMenu actions={this.props.actions}
+              mode={this.props.mode}
+              cells={this.props.cells}
+              viewMode={this.props.viewMode}
+              title={this.props.title}
+              sidePaneMode={this.props.sidePaneMode}
+              lastSaved={this.props.lastSaved} />
+        </div>
+        <div className='presentation-menu' style={{display: (this.props.viewMode === 'presentation' ? 'block' : 'none')}} >
+          <PresentationMenu
+            mode={this.props.mode}
+            cells={this.props.cells}
+            viewMode={this.props.viewMode}
+            title={this.props.title}
+            actions={this.props.actions}
+            sidePaneMode={this.props.sidePaneMode}
+            lastSaved={this.props.lastSaved} />
+        </div>
 
-				<MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <Drawer 
+            width={600} 
+            docked={false}
+            open={this.props.sidePaneMode==='declared variables'} 
+            openSecondary={true}
+            overlayStyle={{backgroundColor: 'none'}}
+            onRequestChange={(open, request) => {this.props.actions.changeSidePaneMode(undefined)} } >
+            <h1 className='overlay-title'>Declared Variables</h1>
+            <DeclaredVariables variables={this.props.declaredVariables} />
+          </Drawer>
+        </MuiThemeProvider>
 
-					<Drawer 
-						width={600} 
-						docked={false}
-						open={this.props.sidePaneMode==='declared variables'} 
-						openSecondary={true}
-						overlayStyle={{backgroundColor: 'none'}}
-						onRequestChange={(open, request) => {this.props.actions.changeSidePaneMode(undefined)} }
-						>
-						<h1 className='overlay-title'>Declared Variables</h1>
-						<DeclaredVariables variables={this.props.declaredVariables} />
-					</Drawer>
-				</ MuiThemeProvider>
-
-
-
-				<MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-
-					<Drawer 
-						width={600} 
-						docked={false}
-						open={this.props.sidePaneMode==='history'} 
-						openSecondary={true}
-						overlayStyle={{backgroundColor: 'none'}}
-						onRequestChange={(open, request) => {this.props.actions.changeSidePaneMode(undefined)}}>
-						<h1 className='overlay-title'>Execution History</h1>
-						<div className='history-cells'>
-							{histContents}
-						</div>
-					</Drawer>
-				</ MuiThemeProvider>
-			</div>
-		)
-	}
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <Drawer 
+            width={600} 
+            docked={false}
+            open={this.props.sidePaneMode==='history'} 
+            openSecondary={true}
+            overlayStyle={{backgroundColor: 'none'}}
+            onRequestChange={(open, request) => {this.props.actions.changeSidePaneMode(undefined)} } >
+            <h1 className='overlay-title'>Execution History</h1>
+            <div className='history-cells'> {histContents} </div>
+          </Drawer>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
 }
 
-{/* <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-
-					<Drawer width={600} open={this.props.sidePaneMode==='history'} >
-						Hello!!!
-					</Drawer>
-
-				</ MuiThemeProvider> */}
 
 class PresentationMenu extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props)
+  }
 
-	render() {
-		return (
-			<div className='presentation-header'>
-				<div className='view-mode-toggle-from-presentation'> 
-				<MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-					<ViewModeToggleButton textColor='black' actions={this.props.actions} viewMode={this.props.viewMode} />
-				</ MuiThemeProvider>
-				</div>
-				<h1 className='presentation-title' style={{color: this.props.title === undefined ? 'gray' : 'black'}}>{this.props.title || 'new notebook'}</h1>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className='presentation-header'>
+        <div className='view-mode-toggle-from-presentation'> 
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <ViewModeToggleButton textColor='black' actions={this.props.actions} viewMode={this.props.viewMode} />
+        </MuiThemeProvider>
+        </div>
+        <h1 className='presentation-title' style={{color: this.props.title === undefined ? 'gray' : 'black'}}>{this.props.title || 'new notebook'}</h1>
+      </div>
+    )
+  }
 }
 
 class EditorMenu extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {previousMode: props.mode}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {previousMode: props.mode}
+  }
 
-	changeSidePaneMode(sidePaneMode) {
-		if (this.props.sidePaneMode === sidePaneMode) this.props.actions.changeSidePaneMode(undefined)
-		else this.props.actions.changeSidePaneMode(sidePaneMode)
-	}
+  changeSidePaneMode(sidePaneMode) {
+    if (this.props.sidePaneMode === sidePaneMode) this.props.actions.changeSidePaneMode(undefined)
+    else this.props.actions.changeSidePaneMode(sidePaneMode)
+  }
 
-	render() {
-		return (
-				<MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-					<Toolbar style={{backgroundColor: 'black'}}>
-							<MainMenu 
-									isFirstChild={true}
-									actions={this.props.actions} 
-									cells={this.props.cells}
-									mode={this.props.mode}
-									title={this.props.title}
-									viewMode={this.props.viewMode}
-									sidePaneMode={this.props.sidePaneMode}
-									lastSaved={this.props.lastSaved}
-									  />
-							<ToolbarGroup className='title-field'>
-								<Title actions={this.props.actions}
-									title={this.props.title}
-									pageMode={this.props.mode} />
-							</ToolbarGroup>
-							<ToolbarGroup id='notebook-view-mode-controls' className='mode-buttons'>
-								<ToolbarTitle style={{fontSize:'13px', color:'lightgray', fontStyle: 'italic'}} text={this.props.lastSaved === undefined ? ' ' : 'last saved: ' + prettyDate(this.props.lastSaved)} />
-								<IconButton 
-									tooltip='Declared Variables'
-									style={{color: '#fafafa'}} 
-									onClick={()=>{this.props.actions.changeSidePaneMode('declared variables')}} >
-								<ArrowDropDown />
-								</IconButton>
-								<IconButton 
-									tooltip='History'
-									style={{color: '#fafafa'}} 
-									onClick={()=>{this.props.actions.changeSidePaneMode('history') }} >
-								<HistoryIcon />
-								</IconButton>
-								<ViewModeToggleButton actions={this.props.actions} viewMode={this.props.viewMode} />
-							</ToolbarGroup>
-							
-					</Toolbar>
-					
-				</MuiThemeProvider>
-		)
-	}
+  render() {
+    return (
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+          <Toolbar style={{backgroundColor: 'black'}}>
+              <MainMenu 
+                  isFirstChild={true}
+                  actions={this.props.actions} 
+                  cells={this.props.cells}
+                  mode={this.props.mode}
+                  title={this.props.title}
+                  viewMode={this.props.viewMode}
+                  sidePaneMode={this.props.sidePaneMode}
+                  lastSaved={this.props.lastSaved}
+                    />
+              <ToolbarGroup className='title-field'>
+                <Title actions={this.props.actions}
+                  title={this.props.title}
+                  pageMode={this.props.mode} />
+              </ToolbarGroup>
+              <ToolbarGroup id='notebook-view-mode-controls' className='mode-buttons'>
+                <ToolbarTitle style={{fontSize:'13px', color:'lightgray', fontStyle: 'italic'}} text={this.props.lastSaved === undefined ? ' ' : 'last saved: ' + prettyDate(this.props.lastSaved)} />
+                <IconButton 
+                  tooltip='Declared Variables'
+                  style={{color: '#fafafa'}} 
+                  onClick={()=>{this.props.actions.changeSidePaneMode('declared variables')}} >
+                <ArrowDropDown />
+                </IconButton>
+                <IconButton 
+                  tooltip='History'
+                  style={{color: '#fafafa'}} 
+                  onClick={()=>{this.props.actions.changeSidePaneMode('history') }} >
+                <HistoryIcon />
+                </IconButton>
+                <ViewModeToggleButton actions={this.props.actions} viewMode={this.props.viewMode} />
+              </ToolbarGroup>
+          </Toolbar>
+        </MuiThemeProvider>
+    )
+  }
 }
 
 
@@ -256,4 +235,28 @@ class ViewModeToggleButton extends React.Component {
     }
 }
 
-export {NotebookHeader}
+// actions={this.props.actions}
+//                 mode={this.props.mode}
+//                 cells={this.props.cells}
+//                 viewMode={this.props.viewMode}
+//                 title={this.props.title}
+//                 declaredVariables={this.props.declaredProperties}
+//                 sidePaneMode={this.props.sidePaneMode}
+//                 lastSaved={this.props.lastSaved}
+//                 history={this.props.history}
+//                 currentTitle={this.props.title} 
+
+function mapStateToProps(state) {
+    // FIXME: don't pass full state
+    // re cells: only pass list of cell ids
+  return state
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotebookHeader)
+// export {NotebookHeader}
