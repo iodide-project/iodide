b3021f641f9f5f1170d269a157d3152d3baaa8d0
bcolloran <bcolloran@mozilla.com>
2017-11-07 11:32:54 -0800

    fixed example import

--- src/example-notebooks.jsx
+++ src/example-notebooks.jsx
@@ -2,7 +2,7 @@ var exampleNotebooks = []
 
 import evictions from '../examples/evictions-by-san-francisco-neighborhood--1999-2015.json'
 import introduction from '../examples/what-does-a-javascript-notebook-look-like-.json'
-import ffusers from '../a-multi-dimensional-view-of-firefox-users.json'
+import ffusers from '../examples/a-multi-dimensional-view-of-firefox-users.json'
 
 exampleNotebooks.push(evictions)
 exampleNotebooks.push(introduction)
