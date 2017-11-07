23232393d91ab93bcd2b3bc0b5fbf537cfa87cfd
bcolloran <bcolloran@mozilla.com>
2017-11-07 11:31:08 -0800

    added FF user example

--- src/example-notebooks.jsx
+++ src/example-notebooks.jsx
@@ -2,10 +2,10 @@ var exampleNotebooks = []
 
 import evictions from '../examples/evictions-by-san-francisco-neighborhood--1999-2015.json'
 import introduction from '../examples/what-does-a-javascript-notebook-look-like-.json'
-
+import ffusers from '../a-multi-dimensional-view-of-firefox-users.json'
 
 exampleNotebooks.push(evictions)
 exampleNotebooks.push(introduction)
-
+exampleNotebooks.push(ffusers)
 
 export default exampleNotebooks
