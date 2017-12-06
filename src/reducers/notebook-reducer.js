600eda5bc1462a0316d76dc5340e0dc9de0ecc9d
hamilton <hamilton.ulmer@gmail.com>
2017-12-05 16:22:03 -0800

    linted / cleaned up the notebook reducer

--- src/reducers/notebook-reducer.js
+++ src/reducers/notebook-reducer.js
@@ -1,4 +1,3 @@
-import * as NB from '../notebook-utils.js'
 import { newNotebook, newCell, blankState } from '../state-prototypes.js'
 
 function clearHistory(loadedState) {
@@ -15,79 +14,90 @@ function clearHistory(loadedState) {
 }
 
 let notebook = function (state = newNotebook(), action) {
+  
+  let nextState
+  let title
+  let cells
+
   switch (action.type) {
   case 'NEW_NOTEBOOK':
-    var newState = newNotebook()
-    return newState
+    return newNotebook()
 
-  case 'EXPORT_NOTEBOOK':
-    var outputState = Object.assign({}, state)
-    clearHistory(outputState)
-    var dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(outputState))
-    var dlAnchorElem = document.getElementById('export-anchor')
+  case 'EXPORT_NOTEBOOK': {
+    nextState = Object.assign({}, state)
+    clearHistory(nextState)
+    let dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(nextState))
+    let dlAnchorElem = document.getElementById('export-anchor')
     dlAnchorElem.setAttribute('href', dataStr)
-    var title = outputState.title === undefined ? 'new-notebook' : outputState.title
-    var filename = title.replace(/[^a-z0-9]/gi, '-').toLowerCase() + '.json'
+    title = nextState.title === undefined ? 'new-notebook' : nextState.title
+    let filename = title.replace(/[^a-z0-9]/gi, '-').toLowerCase() + '.json'
     dlAnchorElem.setAttribute('download', filename)
     dlAnchorElem.click()
+      
     return Object.assign({}, state)
+  }
 
-  case 'IMPORT_NOTEBOOK':
+  case 'IMPORT_NOTEBOOK': {
     // note: loading a NB should always assign to a copy of the latest global
     // and per-cell state for backwards compatibility
-    var loadedState = action.newState
-    clearHistory(loadedState)
-    var cells = loadedState.cells.map(
-      cell => Object.assign(newCell(loadedState.cells, cell.cellType), cell) )
-    return Object.assign(blankState(), loadedState, {cells})
+    nextState = action.newState
+    clearHistory(nextState)
+    cells = nextState.cells.map(
+      cell => Object.assign(newCell(nextState.cells, cell.cellType), cell) )
+    return Object.assign(blankState(), nextState, {cells})
+  }
 
-  case 'SAVE_NOTEBOOK':
-    if (!action.autosave) var lastSaved = new Date()
+  case 'SAVE_NOTEBOOK': {
+    // is this a test
+    // I don't understand
+    let lastSaved
+    if (!action.autosave) lastSaved = new Date()
     else lastSaved = state.lastSaved
-    var outputState = Object.assign({}, state, {lastSaved}, {cells: state.cells.slice().map(c=>{
+    nextState = Object.assign({}, state, {lastSaved}, {cells: state.cells.slice().map(c=>{
       let newC = Object.assign({},c)
       newC.evalStatus = undefined
       if (newC.cellType === 'javascript' || newC.cellType === 'external dependencies') {
         newC.value = undefined
       }
       return newC
-    }
-    ), }, {title: state.title === undefined ? 'new notebook' : state.title})
-    clearHistory(outputState)
-    var title
+    }), }, {title: state.title === undefined ? 'new notebook' : state.title})
+    clearHistory(nextState)
     if (action.title!==undefined) title = action.title
     else title = state.title
-    window.localStorage.setItem(title, JSON.stringify(outputState))
+    window.localStorage.setItem(title, JSON.stringify(nextState))
     return Object.assign({}, state, {lastSaved})
-
-  case 'LOAD_NOTEBOOK':
+  }
+  case 'LOAD_NOTEBOOK': {
     // note: loading a NB should always assign to a copy of the latest global
     // and per-cell state for backwards compatibility
-    var loadedState = JSON.parse(window.localStorage.getItem(action.title))
-    clearHistory(loadedState)
-    var cells = loadedState.cells.map(
-      cell => Object.assign(newCell(loadedState.cells, cell.cellType), cell) )
-    var nextState = Object.assign(blankState(), loadedState, {cells})
+    nextState = JSON.parse(window.localStorage.getItem(action.title))
+    clearHistory(nextState)
+    cells = nextState.cells.map(
+      cell => Object.assign(newCell(nextState.cells, cell.cellType), cell) )
+    nextState = Object.assign(blankState(), nextState, {cells})
     return nextState
-
-  case 'DELETE_NOTEBOOK':
-    var title = action.title
+  }
+  case 'DELETE_NOTEBOOK': {
+    title = action.title
     if (window.localStorage.hasOwnProperty(title)) window.localStorage.removeItem(title)
-    var newState = (title === state.title) ? Object.assign({}, newNotebook()) : Object.assign({}, state)
-    return newState
-
+    nextState = (title === state.title) ? Object.assign({}, newNotebook()) : Object.assign({}, state)
+    return nextState
+  }
   case 'CHANGE_PAGE_TITLE':
     return Object.assign({}, state, {title: action.title})
 
-  case 'SET_VIEW_MODE':
-    var viewMode = action.viewMode
+  case 'SET_VIEW_MODE': {
+    let viewMode = action.viewMode
     return Object.assign({}, state, {viewMode})
-    
-  case 'CHANGE_SIDE_PANE_MODE':
+  }
+      
+  case 'CHANGE_SIDE_PANE_MODE': {
     return Object.assign({}, state, {sidePaneMode: action.mode})
-    
-  default:
+  }
+
+  default: {
     return state
+  }
   }
 }
 
