cdd5e566e350af813668ba031636ec709f25f28d
hamilton <hamilton.ulmer@gmail.com>
2018-01-30 15:24:06 -0800

    unintentionally closes #205 - bootstrap css no longer necessary

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import {render} from 'react-dom'
 
-import 'font-awesome/css/font-awesome.css';
-import 'opensans-npm-webfont';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'codemirror/theme/eclipse.css';
-import 'codemirror/lib/codemirror.css';
-import 'react-table/react-table.css';
-import './page.css';
+import 'font-awesome/css/font-awesome.css'
+import 'opensans-npm-webfont'
+
+import 'codemirror/theme/eclipse.css'
+import 'codemirror/lib/codemirror.css'
+import 'react-table/react-table.css'
+import './page.css'
 
 import Page from './components/page.jsx'
 import {store} from './store.js'
 
-import './tools/nd.js';
+import './tools/nd.js'
 
 render(
   <Provider store={store}>
