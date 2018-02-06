546e159d764d8611d59af458c63290da50abc917
Michael Droettboom <mdboom@gmail.com>
2018-02-06 17:37:57 -0500

    Make eslintrc shim even more robust by passing root dir to Neutrino

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,6 @@ const { Neutrino } = require('neutrino');
 // Specify middleware to Neutrino prior to calling eslintrc.
 // Even if using .neutrinorc.js, you must specify it when using
 // the API
-module.exports = Neutrino()
-  .use(__dirname + '/.neutrinorc.js')
+module.exports = Neutrino({ root: __dirname })
+  .use('/.neutrinorc.js')
   .call('eslintrc');
