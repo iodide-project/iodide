0d5e401168df3b7121694f309a8d8c9512e95e1e
hamilton <hamilton.ulmer@gmail.com>
2018-02-09 08:26:51 -0800

    began refactoring ExternalResourceCell, more work before push

--- src/components/cell-type-external-resource.jsx
+++ src/components/cell-type-external-resource.jsx
@@ -1,82 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import CheckCircle from 'material-ui/svg-icons/action/check-circle'
-import ErrorCircle from 'material-ui/svg-icons/alert/error'
-import UnloadedCircle from 'material-ui/svg-icons/content/remove'
-
 import TwoRowCell from './two-row-cell'
 import CellEditor from './cell-editor'
+import ExternalResourceOutputHandler from './output-handler-external-resource'
 
-import actions from '../actions'
 import { getCellById } from '../notebook-utils'
 
-class ExternalResourceCell extends React.Component {
-  outputComponent = () => {
-    if (this.props.value === undefined) return undefined
-    const outs = this.props.value.filter(d => d.src !== '').map((d, i) => {
-      let statusExplanation
-      let statusIcon
-      let source = d.src.split('/')
-      source = source[source.length - 1]
-
-      const introducedVariables = (d.variables || []).map((v, j) =>
-        (
-          <div
-            key={j}
-            style={{
-            fontSize: '12px',
-            borderRadius: '12px',
-            padding: '3px 8px 3px 8px',
-            marginRight: '6px',
-            backgroundColor: 'lightgray',
-            }}
-          >{v}
-          </div>
-        ))
-
-      if (d.status === undefined) {
-        statusIcon = <UnloadedCircle />
-      } else {
-        statusIcon = (d.status === 'loaded'
-          ? <CheckCircle color="lightblue" />
-          : <ErrorCircle color="firebrick" />
-        )
-      }
-      if (Object.prototype.hasOwnProperty.call(d, 'statusExplanation')) {
-        // TODO: Don't use an array index as a key here (See react/no-array-index-key linter)
-        statusExplanation = <div key={i} className="dependency-status-explanation">{d.statusExplanation}</div>  // eslint-disable-line
-      }
-      return (
-      // TODO: Don't use an array index as a key here (See react/no-array-index-key linter)
-      /* eslint-disable */
-          <div className="dependency-container" key={`erc-${this.props.cellId}-${i}`}>
-            <div className="dependency-row">
-            <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-                {statusIcon}
-            </MuiThemeProvider>
-            <div style={{display:'flex', flexWrap:'wrap', lineHeight: '1.5em'}}>
-              <div className="dependency-src"><a href={d.src} target='_blank'>{source}</a></div>
-              { introducedVariables }
-            </div>
-              
-            </div>
-            {statusExplanation}
-            
-
-          </div>
-        /* eslint-enable */
-      )
-    })
-    return (
-      <div className="dependency-output">
-        {outs}
-      </div>
-    )
+export class ExternalResourceCellUnconnected extends React.Component {
+  static propTypes = {
+    cellId: PropTypes.number.isRequired,
+    value: PropTypes.array,
+    rendered: PropTypes.bool.isRequired, // not required at the moment, but will be soon.
   }
 
   render() {
@@ -84,14 +21,14 @@ class ExternalResourceCell extends React.Component {
       <TwoRowCell
         cellId={this.props.cellId}
         row1={<CellEditor cellId={this.props.cellId} />}
-        row2={this.outputComponent()}
+        row2={<ExternalResourceOutputHandler value={this.props.value} render={this.props.rendered} />}
       />
     )
   }
 }
 
 
-function mapStateToProps(state, ownProps) {
+export function mapStateToProps(state, ownProps) {
   const cell = getCellById(state.cells, ownProps.cellId)
   return {
     value: cell.value,
@@ -99,10 +36,4 @@ function mapStateToProps(state, ownProps) {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ExternalResourceCell)
+export default connect(mapStateToProps)(ExternalResourceCellUnconnected)
