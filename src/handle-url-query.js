f97071256617f56057a3a574bd38e90ded1ba6cf
bcolloran <bcolloran@mozilla.com>
2018-02-20 12:27:08 -0800

    using "query-string" package for parsing url query string

--- src/handle-url-query.js
+++ src/handle-url-query.js
@@ -1,20 +1,8 @@
+import queryString from 'query-string'
+
 import { stateFromJsmd } from './jsmd-tools'
 import { store } from './store'
 import actions from './actions'
-
-function decodeQueryParams(a) {
-  if (a === '') return {};
-  const b = {};
-  for (let i = 0; i < a.length; ++i) {
-    const p = a[i].split('=', 2);
-    if (p.length === 1) {
-      b[p[0]] = ''
-    } else {
-      b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
-    }
-  }
-  return b;
-}
 
 async function loadJsmdFromNotebookUrl(url) {
   try {
@@ -31,7 +19,7 @@ async function loadJsmdFromNotebookUrl(url) {
 
 
 function handleUrlQuery() {
-  const queryParams = decodeQueryParams(window.location.search.substr(1).split('&'));
+  const queryParams = queryString.parse(window.location.search);
   if ({}.hasOwnProperty.call(queryParams, 'url')) {
     loadJsmdFromNotebookUrl(queryParams.url)
   } else if ({}.hasOwnProperty.call(queryParams, 'gist')) {
