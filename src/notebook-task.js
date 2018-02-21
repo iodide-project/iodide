95bd79b91b2330802dca7b0885dc97a613d57552
hamilton <hamilton.ulmer@gmail.com>
2018-02-20 17:18:34 -0800

    cleanup and code review

--- src/notebook-task.js
+++ src/notebook-task.js
@@ -1,7 +1,3 @@
-
-
-/* notebook-task.js */
-
 const TASK_ERRORS = {
   noCallback: 'you must provide either a callback, a keybindingCalblack, or both (if the callbacks differ)',
   noKeybindingsWithCallback: 'keybindingCallback supplied, but no keybindings were declared',
@@ -14,12 +10,6 @@ export { TASK_ERRORS }
 
 export default class NotebookTask {
   constructor(args) {
-    // keybindingCallback + keybindings (both optional)
-    // displayKeybinding (optional)
-    // callback (required)
-    // title (required) / menuLabel (optional)
-    // description (required)
-
     if (!(args instanceof Object && args.constructor === Object)) {
       throw new TypeError(TASK_ERRORS.argumentMustBeObject)
     }
@@ -33,17 +23,9 @@ export default class NotebookTask {
       throw new TypeError(TASK_ERRORS.callbackIsNotFunction)
     }
 
-    // if (args.keybindingCallback) this.keybindingCallback = args.keybindingCallback
-    // if (args.keybindings) this.keybindings = args.keybindings
-
     this.args = args
 
     if (!args.title) throw new TypeError(TASK_ERRORS.noTitleSupplied)
-    // this.title = args.title
-    // if (args.menuTitle) this.menuTitle = args.menuTitle
-
-    // if (args.callback) this.callback = args.callback
-    // if (args.displayKeybinding) this.displayKeybinding = args.displayKeybinding
   }
 
   get keybindingCallback() {
@@ -59,7 +41,7 @@ export default class NotebookTask {
   }
 
   get displayKeybinding() {
-    return this.args.displayKeybinding || ''// this.args.keybindings
+    return this.args.displayKeybinding || ''
   }
 
   get callback() {
@@ -74,57 +56,3 @@ export default class NotebookTask {
     return this.args.menuTitle || this.args.title
   }
 }
-
-/* task-definitions.js */
-
-// const TASKS = {}
-
-// TASKS.AddCellBelow = new NotebookTask({
-//   defaultTitle: 'Add Cell Below',
-//   description: 'adds a cell below the currently-selected cell',
-//   keybindings: ['b'],
-//   callback() {
-//     dispatcher.dispatch()
-//   },
-// })
-
-
-// TASKS.ChangeSelectedCellToJavascript = new NotebookTask({
-//     title: 'Change Selected Cell To Javascript',
-//     menuLabel: 'To Javascript',
-//     tooltip: 'Change To Javascript',
-//     description: 'adds a cell below the currently-selected cell',
-//     keybindings: ['b'],
-//     callback() {
-//       dispatcher.dispatch()
-//     },
-//   })
-
-// TASKS.AddCellBelow.title
-
-// export default TASKS
-
-// /* keybindings - fairly easy to handle. */
-
-// /* some menu item */
-
-// <Menu>
-//     <MenuItem task={TASKS.AddCellBelow} />
-//     <MenuItem task={TASKS.AddCellBelow} />
-// </Menu>
-
-// use NotebookTask in keybindings - just need keybindingCallback.
-// use NotebookTask in menu item - need:
-//     title (always default? can I override? special "menuTitle" field?)
-//     callback
-//     prettyKeybinding (optional)
-
-// var t = new NotebookTask({...})
-// let MenuItems = {}
-// MenuItems.AddCellBelow = <MenuItem name={t.menuTitle} callback={t.callback} />
-
-// å
-// testing:
-
-// - test getters
-// - test callbacks individually
