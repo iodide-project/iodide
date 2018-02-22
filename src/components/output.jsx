95323198ee455028ff725082c5a255203579e568
Michael Droettboom <mdboom@gmail.com>
2018-02-22 09:50:48 -0500

    Use react-inspector instead of JSONTree

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import _ from 'lodash'
-import JSONTree from 'react-json-tree'
 import ReactTable from 'react-table'
+import { ObjectInspector } from 'react-inspector'
 
 import { SimpleTable, makeMatrixText } from './pretty-matrix'
 
@@ -23,25 +23,12 @@ function renderValue(value, inContainer = false) {
       } else if (resultElem.type !== undefined) {
         return resultElem
       } else {
-        console.log('Unknown output handler result type from ' + handler)
         // Fallback to other handlers if it's something invalid
       }
       /* eslint-enable */
     }
   }
   return undefined
-}
-
-const nullHandler = {
-  shouldHandle: value => (value === null),
-
-  render: () => <pre>null</pre>,
-}
-
-const undefinedHandler = {
-  shouldHandle: value => (value === undefined),
-
-  render: () => <pre>undefined</pre>,
 }
 
 const renderMethodHandler = {
@@ -138,67 +125,18 @@ const arrayHandler = {
   },
 }
 
-const dateHandler = {
-  shouldHandle: value => Object.prototype.toString.call(value) === '[object Date]',
-
-  render: value => value.toString(),
-}
-
-const scalarHandler = {
-  scalarTypes: {
-    string: true,
-    number: true,
-  },
-
-  shouldHandle: value => (typeof (value) in scalarHandler.scalarTypes),
-
-  render: value =>
-  // TODO: This probably needs a new CSS class
-    <span className="array-output">{value}</span>,
-
-}
-
 const defaultHandler = {
   shouldHandle: () => true,
 
-  render: value => (
-    <JSONTree
-      data={value}
-      shouldExpandNode={() => false}
-      hideRoot={false}
-      theme={{
-        scheme: 'bright',
-        author: 'chris kempson (http://chriskempson.com)',
-        base00: '#000000',
-        base01: '#303030',
-        base02: '#505050',
-        base03: '#b0b0b0',
-        base04: '#d0d0d0',
-        base05: '#e0e0e0',
-        base06: '#f5f5f5',
-        base07: '#ffffff',
-        base08: '#fb0120',
-        base09: '#fc6d24',
-        base0A: '#fda331',
-        base0B: '#a1c659',
-        base0C: '#76c7b7',
-        base0D: '#6fb3d2',
-        base0E: '#d381c3',
-        base0F: '#be643c',
-      }}
-    />
-  ),
+  render: value => <div><ObjectInspector data={value} /></div>,
+  // render: value => value,
 }
 
 const handlers = [
-  nullHandler,
-  undefinedHandler,
   renderMethodHandler,
   dataFrameHandler,
   matrixHandler,
   arrayHandler,
-  dateHandler,
-  scalarHandler,
   defaultHandler,
 ]
 
