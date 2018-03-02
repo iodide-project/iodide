2f94fd7ae4299746e86aee9b2b74fec4277c9023
bcolloran <bcolloran@mozilla.com>
2018-03-02 13:21:50 -0800

    added wrapping span with title for each array elt

--- src/components/output.jsx
+++ src/components/output.jsx
@@ -108,32 +108,32 @@ const arrayHandler = {
     const dataSetInfo = `${value.length} element array`
     const len = value.length
 
-    const values = []
-
-    function addValues(start, end) {
-      for (let i = start; i < end; i++) {
-        values.push(renderValue(value[i], true))
-        if (i !== end - 1) {
-          values.push(', ')
-        }
-      }
+    let arrayElements = _.range(len > 200 ? 100 : len - 2).map(i => (
+      <span key={`array_elt_${i}`} title={`array index: ${i}`}>
+        {renderValue(value[i], true)}{', '}
+      </span>
+    ))
+    if (len > 200) {
+      arrayElements.push(<span key="array_elts omitted">…, </span>)
+      arrayElements = arrayElements
+        .concat(_.range(len - 100, len - 2)
+          .map(i => (
+            <span key={`array_elt_${i}`} title={`array index: ${i}`}>
+              {renderValue(value[i], true)}{', '}
+            </span>
+          )))
     }
-
-    values.push('[')
-    if (len < 500) {
-      addValues(0, len)
-    } else {
-      addValues(0, 100)
-      values.push(' … ')
-      addValues(len - 100, len)
-    }
-    values.push(']')
-
+    // final element has no trailing comma
+    arrayElements.push((
+      <span key={`array_elt_${len - 1}`} title={`array index: ${len - 1}`}>
+        {renderValue(value[len - 1], true)}
+      </span>
+    ))
     return (
       <div>
         <div className="data-set-info">{dataSetInfo}</div>
         <div>
-          {values}
+          [{arrayElements}]
         </div>
       </div>
     )
