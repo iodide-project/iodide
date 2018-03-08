a3f371376524b8c1ced9563ec42aaba8d92f470b
hamilton <hamilton.ulmer@gmail.com>
2018-03-07 18:20:37 -0800

    npm run lint was missing

--- src/components/toolbar/editor-mode-title.jsx
+++ src/components/toolbar/editor-mode-title.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import { connect } from 'react-redux'
-import Tooltip from 'material-ui/Tooltip'
 
 import tasks from '../../task-definitions'
 
