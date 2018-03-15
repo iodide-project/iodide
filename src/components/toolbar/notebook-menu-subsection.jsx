47cc8ac5770787e7bc1288983983cf608ab8d259
bcolloran <bcolloran@mozilla.com>
2018-03-15 00:00:53 -0700

    Revert "Closed multiple windows on clicking outside menu"

--- src/components/toolbar/notebook-menu-subsection.jsx
+++ src/components/toolbar/notebook-menu-subsection.jsx
@@ -19,7 +19,6 @@ export default class NotebookMenuSubsection extends React.Component {
 
   handleClose() {
     this.setState({ anchorElement: null })
-    if (this.props.onClick) this.props.onClick()
   }
   render() {
     const { anchorElement } = this.state
