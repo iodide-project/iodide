7f0fe28c98dd03a0f790d6eb1d7edb5d75c7e772
Shubham Singh <imshubhamsingh97@gmail.com>
2018-03-28 05:27:36 +0530

    fix for 516

--- src/components/pretty-matrix.jsx
+++ src/components/pretty-matrix.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
+import JSONTree from 'react-json-tree'
 import nb from '../tools/nb'
+
 
 function cellText(matrixLike, i, j) {
   const [numRows, numCols] = nb.shape(matrixLike)
@@ -92,7 +94,7 @@ class PrettyMatrix extends React.Component {
             (<tr key={iTr}>
               {colInds.map((j, jTd) =>
                 <td key={jTd}>{this.cellText(i, j)}</td>)}
-             </tr>))}
+            </tr>))}
         </tbody>
       </table>
     )
@@ -102,8 +104,39 @@ class PrettyMatrix extends React.Component {
 
 
 class SimpleTable extends React.Component {
+  /* eslint-disable */
+  columntext(colText) {
+    if (_.isObject(colText)) {
+      return <JSONTree
+        data={colText}
+        hideRoot={false}
+        theme={{
+          scheme: 'bright',
+          author: 'chris kempson (http://chriskempson.com)',
+          base00: '#000000',
+          base01: '#303030',
+          base02: '#505050',
+          base03: '#b0b0b0',
+          base04: '#d0d0d0',
+          base05: '#e0e0e0',
+          base06: '#f5f5f5',
+          base07: '#ffffff',
+          base08: '#fb0120',
+          base09: '#fc6d24',
+          base0A: '#fda331',
+          base0B: '#a1c659',
+          base0C: '#76c7b7',
+          base0D: '#6fb3d2',
+          base0E: '#d381c3',
+          base0F: '#be643c',
+        }}
+      />
+    } else {
+      return colText
+    }
+  }
+
   render() {
-    /* eslint-disable */
     return (
       <table className="matrixTable">
         <tbody>
@@ -111,8 +144,8 @@ class SimpleTable extends React.Component {
             // TODO: Do not use array indices in keys
             (<tr key={iTr}>
               {row.map((colText, jTd) =>
-                <td key={jTd}>{colText}</td>)}
-             </tr>))}
+                <td key={jTd}>{this.columntext(colText)}</td>)}
+            </tr>))}
         </tbody>
       </table>
     )
