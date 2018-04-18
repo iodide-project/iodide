e563ca6cfbf90072e59077092f4ab2d173d97164
hamilton <hamilton.ulmer@gmail.com>
2018-04-18 08:56:44 -0700

    domElementHandler now uses DOMInspector from react-inspector

--- src/components/reps/dom-element-handler.js
+++ src/components/reps/dom-element-handler.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import DOMInspector from 'react-inspector'
 // taken from https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
 function isNode(o) {
   return (
@@ -17,5 +17,5 @@ function isElement(o) {
 
 export default {
   shouldHandle: value => isNode(value) || isElement(value),
-  render: value => <div dangerouslySetInnerHTML={{ __html: value.outerHTML }}></div>, // eslint-disable-line
+  render: value => <DOMInspector data={value} />,
 }
