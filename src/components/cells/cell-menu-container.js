bc9b6738d40ac07bea5b3b44bdb6cfbbfa9cac4f
bcolloran <bcolloran@mozilla.com>
2018-05-26 14:01:20 -0700

    changed carrot icon

--- src/components/cells/cell-menu-container.js
+++ src/components/cells/cell-menu-container.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import Tooltip from 'material-ui/Tooltip'
 import Menu from 'material-ui/Menu'
-import ExpandMore from 'material-ui-icons/ExpandMore'
+import ArrowDropDown from 'material-ui-icons/ArrowDropDown'
 import CellMenu from './cell-menu'
 
 import * as actions from '../../actions/actions'
@@ -83,7 +83,7 @@ export class CellMenuContainerUnconnected extends React.Component {
               onClick={this.handleClick}
             >
               {this.props.label}
-              <ExpandMore />
+              <ArrowDropDown style={{ fontSize: 20 }} />
 
             </div>
           </React.Fragment>
