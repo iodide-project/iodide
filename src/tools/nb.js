61f947e041e7127e92e9af90b60eeb75c0d847f4
hamilton <hamilton.ulmer@gmail.com>
2018-06-19 15:08:47 -0700

    further tests for nb

--- src/tools/nb.js
+++ src/tools/nb.js
@@ -2,12 +2,15 @@ import _ from 'lodash'
 
 let nb = {}
 
+// UNUSED OUTSIDE OF NB.JS fcns that are not used.
 nb.all = function(arr){
   for (let i=0; i<arr.length; i++) {
     if (!arr[i]){return false}
   }
   return true
 }
+
+// UNUSED
 nb.any = function(arr){
   for (let i=0; i<arr.length; i++) {
     if (arr[i]){return true}
@@ -15,12 +18,7 @@ nb.any = function(arr){
   return false
 }
 
-// nb.isValuesArray = function(obj){
-//   if (nb.isArray(obj)){
-//     return nb.all(obj.map(x => typ))
-//   } else {return false}
-// }
-
+// only place where nb.all is claled, isn't used. REMOVE
 nb.isMatrixLike = function(obj){
   if (nb.isArray(obj)){
     return nb.all(obj.map(nb.isArray))
@@ -38,6 +36,8 @@ nb.arrayEqual = function(a1,a2){
 nb.sameKeys = function(x,y){
   return nb.arrayEqual(_.sortBy(_.keys(x)), _.sortBy(_.keys(y)))
 }
+
+// isRowDF, isColumnDF, shape
 
 nb.isRowDf = function(obj,rowsToCheck = 100){
   if (!_.isArray(obj) || obj.length==0 || (obj.length === 1 && !_.isPlainObject(obj[0]))) {return false}
@@ -61,35 +61,15 @@ nb.isMatrix = function(obj,rowsToCheck = 100){
 
 nb.isColumnDf = function(obj,colsToCheck = 100){
   if (!_.isPlainObject(obj)) {return false}
+  if (_.isEmpty(obj)) {return false}
   colsToCheck = Math.min(colsToCheck,_.size(obj))
   let cols = Object.keys(obj)
   let numRows = obj[cols[0]].length
   for (let i=1; i<colsToCheck; i++){
-    if (obj[cols[i]].length != numRows)
+    if (!Array.isArray(obj[cols[i]]) || obj[cols[i]].length != numRows)
       return false
   }
   return true
-}
-
-
-nb.nanComparison = function(a,b){
-  if (!isNaN(a-b)) {return a<b ? -1 : 1} //use numeric sort
-  else {return isNaN(a) ? 1:-1}  // if a is nan, sort it *after* b
-}
-  
-nb.rank = function(arr) {
-  let rank = 1
-  let rankMapping = {}
-  arr.slice()
-    .sort(nb.nanComparison) // sort arr pairs (pushing NaNs to end)
-    .forEach( (x,i) => { //build up the rankMapping
-      if (i===0){ rankMapping[x]=rank }
-      if (i>0){
-        if (x>arr[i-1]){rank++}
-        rankMapping[x]=rank
-      }  
-    })
-  return arr.map(item => isNaN(item) ? NaN : rankMapping[item] )
 }
 
 nb.toColumns = function(rowDF){
@@ -108,7 +88,6 @@ nb.toRows = function(colDf){
 }
 
 nb.shape = function(obj){
-  // rows by cols
   if (nb.isColumnDf(obj)){
     let cols = _.keys(obj)
     return [obj[cols[0]].length, cols.length]
@@ -121,12 +100,8 @@ nb.shape = function(obj){
   } 
 }
 
-nb.dropRowsWithNaN = function(col1,col2){
-  return _.unzip( _.zip(col1,col2).filter(xy => !(_.isNaN(xy[0]) || _.isNaN(xy[1]))) )
-}
-
 nb.prettyFormatNumber = function(x,numChars=8){
-  numChars = numChars<=4 ? 4 : numChars
+  numChars = numChars <= 4 ? 4 : numChars
   if(!_.isNumber(x)){return x}
   let str = x.toString()
   if (str.length<=numChars) {return str}
@@ -140,16 +115,6 @@ nb.prettyFormatNumber = function(x,numChars=8){
   } else {
     return x.toFixed(Math.max(0, numChars-intStr.length-2))
   }
-}
-
-nb.cartesianProduct = function(a1,a2){
-  let out = []
-  for (let x of a1){
-    for (let y of a2){
-      out.push([x,y])
-    }
-  }
-  return out
 }
 
 export default nb
