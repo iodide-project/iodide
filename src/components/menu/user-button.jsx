d828f94cd6d458827e7c819060291bbd0855dce0
William Lachance <wlachance@mozilla.com>
2018-06-22 14:28:26 -0400

    Add a react component for the user button

---
+++ src/components/menu/user-button.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+import AccountCircle from 'material-ui-icons/AccountCircle'
+
+import NotebookTaskButton from './notebook-task-button'
+import tasks from '../../actions/task-definitions'
+
+export default class UserButton extends React.Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+  }
+
+  render() {
+    console.log('hi');
+    if (!this.props.isAuthenticated) {
+      return (
+        <NotebookTaskButton task={tasks.logoutGithub}>
+          <img src={this.props.avatar} alt="" className="user-avatar" />
+        </NotebookTaskButton>
+      )
+    }
+
+    // not authenticated, just return a button inviting them to log in
+    return (
+      <NotebookTaskButton task={tasks.loginGithub}>
+        <AccountCircle />
+      </NotebookTaskButton>
+    )
+  }
+}
