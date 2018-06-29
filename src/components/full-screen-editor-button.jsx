8aee46cf791bdfd1447ba17ac8d2d8514593b197
hamilton <hamilton.ulmer@gmail.com>
2018-06-29 16:59:40 -0700

    added tooltip to full screen editor button

--- src/components/full-screen-editor-button.jsx
+++ src/components/full-screen-editor-button.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Tooltip from 'material-ui/Tooltip'
 
 import tasks from '../actions/task-definitions'
 
 export class FullScreenEditorButtonUnconnected extends React.Component {
   render() {
     return (
-      <button
-        className="full-screen-editor-button"
-        onClick={() => { tasks.toggleEvalFrameVisibility.callback(); }}
-      >{this.props.showFrame ? '→' : '←'}
-      </button>
+      <Tooltip classes={{ tooltip: 'iodide-tooltip' }} title="full width editor">
+        <button
+          className="full-screen-editor-button"
+          onClick={() => { tasks.toggleEvalFrameVisibility.callback(); }}
+        >{this.props.showFrame ? '→' : '←'}
+        </button>
+      </Tooltip>
     )
   }
 }
