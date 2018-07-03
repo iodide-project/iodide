1938577892d7f03750c59753dd65df4780a69b93
Daniela Carey <danielafcarey@gmail.com>
2018-07-03 10:54:50 -0600

    Set up CellMenuContainerUnconnected test file and exports

---
+++ src/components/cells/__tests__/cell-menu-container.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {
+  CellMenuContainerUnconnected,
+  mapStateToProps,
+  mapDispatchToProps
+} from '../cell-menu-container';
+
+describe('CellMenuContainerUnconnected', () => {
+
+})
+
+describe('CellMenuContainerUnconnected mapStateToProps', () => {
+
+})
+
+describe('CellMenuContainerUnconnected mapDispatchToProps', () => {
+
+})
