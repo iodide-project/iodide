2917f8635aa40acf02e5d9535188c8ae4532ead2
Daniela Carey, Camilo Snapp, Cameron Buscher <danielafcarey@gmail.com, camilo.snapp@gmail.com, cameronbuscher@gmail.com>
2018-07-05 14:48:12 -0600

    Fix linting errors

--- src/components/cells/__tests__/cell-menu-container.test.js
+++ src/components/cells/__tests__/cell-menu-container.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import {
   CellMenuContainerUnconnected,
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 } from '../cell-menu-container';
 
 import * as notebookUtils from '../../../tools/notebook-utils'
@@ -15,40 +15,40 @@ describe('CellMenuContainerUnconnected', () => {
     props = {
       label: 'md',
       cellId: 0,
-      skipInRunAll: false 
-    } 
+      skipInRunAll: false,
+    }
   })
 
   it('has default state of anchorElement as null', () => {
-    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected { ...props } />);
+    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected {...props} />);
 
     expect(mountedMenuContainer.state('anchorElement')).toEqual(null);
   })
 
   it('matches the snapshot if props.skipInRunAll is false and anchorElement is null', () => {
-    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected { ...props } />);
+    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected {...props} />);
 
     expect(mountedMenuContainer).toMatchSnapshot();
   })
 
   it('matches the snapshot if props.skipInRunAll is true', () => {
     props.skipInRunAll = true;
-    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected { ...props } />);
+    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected {...props} />);
 
     expect(mountedMenuContainer).toMatchSnapshot();
   })
 
   it('matches the snapshot if state.anchorElement is not null', () => {
-    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected { ...props } />);
+    const mountedMenuContainer = shallow(<CellMenuContainerUnconnected {...props} />);
     mountedMenuContainer.setState({ anchorElement: 'not null' })
     mountedMenuContainer.update();
 
-    expect(mountedMenuContainer).toMatchSnapshot(); 
+    expect(mountedMenuContainer).toMatchSnapshot();
   })
 
   describe('handleClick', () => {
     it('updates anchorElement in state with the currentTarget of the clicked element', () => {
-      const mountedMenuContainer = shallow(<CellMenuContainerUnconnected { ...props } />);
+      const mountedMenuContainer = shallow(<CellMenuContainerUnconnected {...props} />);
 
       mountedMenuContainer.find('.cell-type-label').simulate('click', { currentTarget: 'a target' });
 
@@ -58,8 +58,8 @@ describe('CellMenuContainerUnconnected', () => {
 
   describe('handleIconButtonClose', () => {
     it('updates anchorElement in state to null', () => {
-      const mountedMenuContainer = shallow(<CellMenuContainerUnconnected { ...props } />);
-      
+      const mountedMenuContainer = shallow(<CellMenuContainerUnconnected {...props} />);
+
       mountedMenuContainer.find('#cell-menu').simulate('close');
 
       expect(mountedMenuContainer.state('anchorElement')).toBeNull();
@@ -73,25 +73,25 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
 
   beforeEach(() => {
     state = {
-      cells: [{ 
+      cells: [{
         asyncProcessCount: 0,
-        cellType: "code",
-        content: "",
-        evalStatus: "UNEVALUATED",
-        executionStatus: " ",
+        cellType: 'code',
+        content: '',
+        evalStatus: 'UNEVALUATED',
+        executionStatus: ' ',
         id: 0,
-        language: "js",
+        language: 'js',
         rendered: false,
         selected: true,
         skipInRunAll: false,
-        value: undefined
+        value: undefined,
       }],
-      title: 'untitled'
-    };   
+      title: 'untitled',
+    };
     ownProps = { cellId: 0 };
-    notebookUtils.getCellById = jest.fn().mockImplementation(() => state.cells[0]);    
+    notebookUtils.getCellById = jest.fn().mockImplementation(() => state.cells[0]);
   })
-  
+
   it('calls getCellById with the correct arguments', () => {
     mapStateToProps(state, ownProps);
 
@@ -101,7 +101,7 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
   it('returns an object with correct label and skipInRunAll key value pairs if cellType is "code"', () => {
     const expected = {
       label: 'js',
-      skipInRunAll: false
+      skipInRunAll: false,
     };
     const result = mapStateToProps(state, ownProps);
 
@@ -109,10 +109,10 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
   })
 
   it('returns an object with correct label and skipInRunAll key value pairs if cellType is "markdown"', () => {
-    state.cells[0].cellType = 'markdown'; 
+    state.cells[0].cellType = 'markdown';
     const expected = {
       label: 'md',
-      skipInRunAll: false
+      skipInRunAll: false,
     };
     const result = mapStateToProps(state, ownProps);
 
@@ -120,10 +120,10 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
   })
 
   it('returns an object with correct label and skipInRunAll key value pairs if cellType is "css"', () => {
-    state.cells[0].cellType = 'css'; 
+    state.cells[0].cellType = 'css';
     const expected = {
       label: 'css',
-      skipInRunAll: false
+      skipInRunAll: false,
     };
     const result = mapStateToProps(state, ownProps);
 
@@ -131,10 +131,10 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
   })
 
   it('returns an object with correct label and skipInRunAll key value pairs if cellType is "plugin"', () => {
-    state.cells[0].cellType = 'plugin'; 
+    state.cells[0].cellType = 'plugin';
     const expected = {
       label: 'plugin',
-      skipInRunAll: false
+      skipInRunAll: false,
     };
     const result = mapStateToProps(state, ownProps);
 
@@ -142,10 +142,10 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
   })
 
   it('returns an object with correct label and skipInRunAll key value pairs if cellType is "external dependencies"', () => {
-    state.cells[0].cellType = 'external dependencies'; 
+    state.cells[0].cellType = 'external dependencies';
     const expected = {
       label: 'resource',
-      skipInRunAll: false
+      skipInRunAll: false,
     };
     const result = mapStateToProps(state, ownProps);
 
@@ -153,22 +153,20 @@ describe('CellMenuContainerUnconnected mapStateToProps', () => {
   })
 
   it('returns an object with correct label and skipInRunAll key value pairs if cellType is "raw"', () => {
-    state.cells[0].cellType = 'raw'; 
+    state.cells[0].cellType = 'raw';
     const expected = {
       label: 'raw',
-      skipInRunAll: false
+      skipInRunAll: false,
     };
     const result = mapStateToProps(state, ownProps);
 
     expect(result).toEqual(expected);
   })
-
 })
 
 describe('mapDispatchToProps', () => {
   it('returns an object with actions as a property', () => {
     const dispatch = jest.fn();
-    const actions = {};
     const result = mapDispatchToProps(dispatch);
 
     expect(result).toHaveProperty('actions');
