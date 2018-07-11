054d7648d3691b2a26035c3cb8dca85403871d93
William Lachance <wlachance@mozilla.com>
2018-07-11 16:57:58 -0400

    Persist github info across requests using a model + middleware

---
+++ server/github/middleware.py
@@ -0,0 +1,25 @@
+import requests
+from social_django.models import UserSocialAuth
+
+
+class GithubAuthMiddleware(object):
+    '''
+    Simple middleware to get the user's avatar url from github (if not already
+    set)
+    '''
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated and not request.user.avatar:
+            user_social_auth = UserSocialAuth.objects.get(user=request.user)
+            social_auth_extra_data = user_social_auth.extra_data
+            github_info = requests.get('https://api.github.com/users/%s' %
+                                       social_auth_extra_data['login']).json()
+            avatar_url = github_info.get('avatar_url')
+            if avatar_url:
+                request.user.avatar = avatar_url
+                request.user.save()
+
+        return self.get_response(request)
--- server/urls.py
+++ server/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
-from django.urls import path
-
 from django.contrib import admin
-admin.autodiscover()
+from django.urls import path
 
 import server.views
 
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', server.views.index, name='index'),
