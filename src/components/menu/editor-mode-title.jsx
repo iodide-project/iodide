31524b040095f50ba236c4ad7c9aab8bead9487d
hamilton <hamilton.ulmer@gmail.com>
2018-07-17 15:55:57 -0700

    missed a PropType change

--- src/components/menu/editor-mode-title.jsx
+++ src/components/menu/editor-mode-title.jsx
@@ -8,7 +8,7 @@ import tasks from '../../actions/task-definitions'
 
 export class TitleUnconnected extends React.Component {
   static propTypes = {
-    pageMode: PropTypes.oneOf(['command', 'edit', 'APP_MODE']),
+    pageMode: PropTypes.oneOf(['COMMAND_MODE', 'EDIT_MODE', 'APP_MODE']),
     title: PropTypes.string,
     textColor: PropTypes.string,
     hoverColor: PropTypes.string,
