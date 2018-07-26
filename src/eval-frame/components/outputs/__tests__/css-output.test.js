a9d4c89e031283e3a1dda70aafade276e4685f7a
bcolloran <bcolloran@mozilla.com>
2018-07-26 13:07:11 -0700

    completely hide css cells from report view

--- src/eval-frame/components/outputs/__tests__/css-output.test.js
+++ src/eval-frame/components/outputs/__tests__/css-output.test.js
@@ -1,75 +1,8 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import { mapStateToProps } from '../css-output'
 
-import {
-  CSSOutputUnconnected as CSSOutput,
-  mapStateToProps,
-} from '../css-output'
-import OutputContainer from '../output-container'
-import OutputRow from '../output-row'
-
+// note: purely declarative component doesn't need testing
 
 describe('CSSOutput_unconnected react component', () => {
-  let props
-  let mountedCell
-  const output = () => {
-    if (!mountedCell) {
-      mountedCell = shallow(<CSSOutput {...props} />)
-    }
-    return mountedCell
-  }
-
-  beforeEach(() => {
-    props = {
-      cellId: 5,
-      value: 'h1 {color:pink}',
-      rendered: false,
-    }
-    mountedCell = undefined
-  })
-
-  it('always renders one OutputContainer', () => {
-    expect(output().find(OutputContainer).length).toBe(1)
-  })
-
-  it('always renders one OutputRow inside OutputContainer', () => {
-    expect(output().wrap(output().find(OutputContainer))
-      .find(OutputRow)).toHaveLength(1)
-  })
-
-  it('is a OutputRow with 1 children', () => {
-    expect(output().find(OutputRow).children().length).toBe(1)
-  })
-
-  it('always renders one style elt inside OutputRow', () => {
-    expect(output().find('style').parent().is(OutputRow)).toEqual(true)
-  })
-
-  it('the style elt has the correct value if rendered===true', () => {
-    props.rendered = true
-    const styleElts = output().find('style')
-    expect(styleElts.text()).toEqual('h1 {color:pink}')
-  })
-
-  it('the style elt is empty if rendered==false', () => {
-    const styleElts = output().find('style')
-    expect(styleElts.text()).toEqual('')
-  })
-
-  it("sets the OutputContainer cellId prop to be the CSSOutput's cellId prop", () => {
-    expect(output().find(OutputContainer).props().cellId)
-      .toBe(props.cellId)
-  })
-
-  it("sets the OutputRow cellId prop to be the CSSOutput's cellId prop", () => {
-    expect(output().find(OutputRow).props().cellId)
-      .toBe(props.cellId)
-  })
-
-  it('sets the OutputRow rowType prop to be input', () => {
-    expect(output().find(OutputRow).props().rowType)
-      .toBe('input')
-  })
 })
 
 
@@ -80,15 +13,15 @@ describe('CSSOutput mapStateToProps', () => {
       { id: 3, value: 'h1 {color:blue}', rendered: false }],
   }
 
-  it('should return the content of the correct cells', () => {
+  it('rendered cells return correct style prop', () => {
     const ownProps = { cellId: 5 }
     expect(mapStateToProps(state, ownProps))
-      .toEqual({ value: 'h1 {color:pink}', rendered: true, cellId: 5 })
+      .toEqual({ style: 'h1 {color:pink}' })
   })
 
-  it('should return the content of the correct cells', () => {
+  it('unrendered cells have empty style prop', () => {
     const ownProps = { cellId: 3 }
     expect(mapStateToProps(state, ownProps))
-      .toEqual({ value: 'h1 {color:blue}', rendered: false, cellId: 3 })
+      .toEqual({ style: '' })
   })
 })
--- src/eval-frame/components/outputs/css-output.jsx
+++ src/eval-frame/components/outputs/css-output.jsx
@@ -2,28 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import OutputRow from './output-row'
-import OutputContainer from './output-container'
-
 import { getCellById } from '../../tools/notebook-utils'
 
 export class CSSOutputUnconnected extends React.Component {
   static propTypes = {
-    cellId: PropTypes.number.isRequired,
-    value: PropTypes.any,
-    rendered: PropTypes.bool.isRequired,
+    style: PropTypes.string.isRequired,
   }
 
   render() {
-    return (
-      <OutputContainer cellId={this.props.cellId}>
-        <OutputRow cellId={this.props.cellId} rowType="input">
-          <style>
-            {this.props.rendered && this.props.value}
-          </style>
-        </OutputRow>
-      </OutputContainer>
-    )
+    return <style>{this.props.style}</style>
   }
 }
 
@@ -31,9 +18,7 @@ export class CSSOutputUnconnected extends React.Component {
 export function mapStateToProps(state, ownProps) {
   const cell = getCellById(state.cells, ownProps.cellId)
   return {
-    cellId: cell.id,
-    value: cell.value,
-    rendered: cell.rendered,
+    style: cell.rendered ? cell.value : '',
   }
 }
 
