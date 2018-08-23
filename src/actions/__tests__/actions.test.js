418e99334eb3d75dde8fea5544a375d03e6f139a
Quinlan Hill <quinhill@icloud.com>
2018-08-23 14:45:54 -0600

    Add more tests to action creator test suite

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -2,485 +2,408 @@ import * as actions from '../actions';
 
 
 describe('Actions', () => {
+  describe('exportNotebook', () => {
+    it('should return the default value', () => {
+      const status = false;
 
-	describe('exportNotebook', () => {
+      const expected = {
+        type: 'EXPORT_NOTEBOOK',
+        exportAsReport: false,
+      };
 
-		it('should return the default value', () => {
+      const results = actions.exportNotebook();
 
-			const status = false;
+      expect(results).toEqual(expected)
+    })
 
-			const expected = {
-				type: 'EXPORT_NOTEBOOK',
-				exportAsReport: false
-			};
+    it('should have a type of "EXPORT_NOTEBOOK"', () => {
+      const expected = {
+        type: 'EXPORT_NOTEBOOK',
+        exportAsReport: true,
+      };
 
-			const results = actions.exportNotebook();
+      const results = actions.exportNotebook(true);
 
-			expect(results).toEqual(expected)
-		})
+      expect(results).toEqual(expected)
+    });
+  });
 
-		it('should have a type of "EXPORT_NOTEBOOK"', () => {
+  describe('saveNotebook', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'SAVE_NOTEBOOK',
+        autosave: false,
+      };
 
-			const status = true;
+      const results = actions.saveNotebook();
 
-			const expected = {
-				type: 'EXPORT_NOTEBOOK',
-				exportAsReport: true
-			};
+      expect(results).toEqual(expected)
+    })
 
-			const results = actions.exportNotebook(true);
+    it('should have a type of "SAVE_NOTEBOOK"', () => {
+      const status = true;
 
-			expect(results).toEqual(expected)
-		});
+      const expected = {
+        type: 'SAVE_NOTEBOOK',
+        autosave: true,
+      };
 
-	});
+      const results = actions.saveNotebook(status);
 
-	describe('saveNotebook', () => {
+      expect(results).toEqual(expected)
+    });
+  });
 
-		it('should return the default value', () => {
+  describe('loadNotebook', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'LOAD_NOTEBOOK',
+        title: undefined,
+      };
 
-			const status = false;
+      const results = actions.loadNotebook();
 
-			const expected = {
-				type: 'SAVE_NOTEBOOK',
-				autosave: false
-			};
+      expect(results).toEqual(expected)
+    })
 
-			const results = actions.saveNotebook();
+    it('should have a type of "LOAD_NOTEBOOK"', () => {
+      const status = 'title string';
 
-			expect(results).toEqual(expected)
-		})
+      const expected = {
+        type: 'LOAD_NOTEBOOK',
+        title: status,
+      };
 
-		it('should have a type of "SAVE_NOTEBOOK"', () => {
+      const results = actions.loadNotebook(status);
 
-			const status = true;
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const expected = {
-				type: 'SAVE_NOTEBOOK',
-				autosave: true
-			};
+  describe('deleteNotebook', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'DELETE_NOTEBOOK',
+        title: undefined,
+      };
 
-			const results = actions.saveNotebook(true);
+      const results = actions.deleteNotebook();
 
-			expect(results).toEqual(expected)
-		});
+      expect(results).toEqual(expected)
+    })
 
-	});
+    it('should have a type of "DELETE_NOTEBOOK"', () => {
+      const status = 'delete title string';
 
-	describe('loadNotebook', () => {
+      const expected = {
+        type: 'DELETE_NOTEBOOK',
+        title: status,
+      };
 
-		it('should return the default value', () => {
+      const results = actions.deleteNotebook(status);
 
-			const expected = {
-				type: 'LOAD_NOTEBOOK',
-				title: undefined
-			};
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const results = actions.loadNotebook();
+  describe('newNotebook', () => {
+    it('should have a type of "NEW_NOTEBOOK"', () => {
+      const expected = {
+        type: 'NEW_NOTEBOOK',
+      };
 
-			expect(results).toEqual(expected)
-		})
+      const results = actions.newNotebook('foo');
 
-		it('should have a type of "LOAD_NOTEBOOK"', () => {
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const status = 'title string';
 
-			const expected = {
-				type: 'LOAD_NOTEBOOK',
-				title: status
-			};
+  describe('clearVariables', () => {
+    it('should have a type of "CLEAR_VARIABLE"', () => {
+      const expected = {
+        type: 'CLEAR_VARIABLES',
+      };
 
-			const results = actions.loadNotebook(status);
+      const results = actions.clearVariables('foo');
 
-			expect(results).toEqual(expected)
-		});
+      expect(results).toEqual(expected)
+    });
+  });
 
-	});
+  describe('changePageTitle', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'CHANGE_PAGE_TITLE',
+        title: undefined,
+      };
 
-	describe('deleteNotebook', () => {
+      const results = actions.changePageTitle();
 
-		it('should return the default value', () => {
+      expect(results).toEqual(expected)
+    })
 
-			const expected = {
-				type: 'DELETE_NOTEBOOK',
-				title: undefined
-			};
+    it('should have a type of "CHANGE_PAGE_TITLE"', () => {
+      const status = 'new title string';
 
-			const results = actions.deleteNotebook();
+      const expected = {
+        type: 'CHANGE_PAGE_TITLE',
+        title: status,
+      };
 
-			expect(results).toEqual(expected)
-		})
+      const results = actions.changePageTitle(status);
 
-		it('should have a type of "DELETE_NOTEBOOK"', () => {
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const status = 'delete title string';
+  describe('changeMode', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'CHANGE_MODE',
+        mode: undefined,
+      };
 
-			const expected = {
-				type: 'DELETE_NOTEBOOK',
-				title: status
-			};
+      const results = actions.changeMode();
 
-			const results = actions.deleteNotebook(status);
+      expect(results).toEqual(expected)
+    })
 
-			expect(results).toEqual(expected)
-		});
+    it('should have a type of "CHANGE_MODE"', () => {
+      const status = 'new mode';
 
-	});
+      const expected = {
+        type: 'CHANGE_MODE',
+        mode: status,
+      };
 
-	describe('newNotebook', () => {
+      const results = actions.changeMode(status);
 
-		it('should have a type of "NEW_NOTEBOOK"', () => {
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const expected = {
-				type: 'NEW_NOTEBOOK',
-			};
+  describe('setViewMode', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'SET_VIEW_MODE',
+        viewMode: undefined,
+      };
 
-			const results = actions.newNotebook('foo');
+      const results = actions.setViewMode();
 
-			expect(results).toEqual(expected)
-		});
+      expect(results).toEqual(expected)
+    })
 
-	});
+    it('should have a type of "SET_VIEW_MODE"', () => {
+      const status = 'new mode';
 
+      const expected = {
+        type: 'SET_VIEW_MODE',
+        viewMode: status,
+      };
 
-	describe('clearVariables', () => {
+      const results = actions.setViewMode(status);
 
-		it('should have a type of "CLEAR_VARIABLE"', () => {
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const expected = {
-				type: 'CLEAR_VARIABLES',
-			};
+  describe('updateInputContent', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'UPDATE_INPUT_CONTENT',
+        content: undefined,
+      };
 
-			const results = actions.clearVariables('foo');
+      const results = actions.updateInputContent();
 
-			expect(results).toEqual(expected)
-		});
+      expect(results).toEqual(expected)
+    })
 
-	});
+    it('should have a type of "UPDATE_INPUT_CONTENT"', () => {
+      const text = 'content text';
 
-	describe('changePageTitle', () => {
+      const expected = {
+        type: 'UPDATE_INPUT_CONTENT',
+        content: text,
+      };
 
-		it('should return the default value', () => {
+      const results = actions.updateInputContent(text);
 
-			const expected = {
-				type: 'CHANGE_PAGE_TITLE',
-				title: undefined
-			};
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const results = actions.changePageTitle();
+  describe('updateCellProperties', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'UPDATE_CELL_PROPERTIES',
+        cellId: undefined,
+        updatedProperties: undefined,
+      };
 
-			expect(results).toEqual(expected)
-		})
+      const results = actions.updateCellProperties();
 
-		it('should have a type of "CHANGE_PAGE_TITLE"', () => {
+      expect(results).toEqual(expected)
+    })
 
-			const status = 'new title string';
+    it('should have a type of "UPDATE_INPUT_CONTENT"', () => {
+      const cellId = 1;
+      const updatedProperties = []
 
-			const expected = {
-				type: 'CHANGE_PAGE_TITLE',
-				title: status
-			};
+      const expected = {
+        type: 'UPDATE_CELL_PROPERTIES',
+        cellId,
+        updatedProperties,
+      };
 
-			const results = actions.changePageTitle(status);
+      const results = actions.updateCellProperties(cellId, updatedProperties);
 
-			expect(results).toEqual(expected)
-		});
+      expect(results).toEqual(expected)
+    });
+  });
 
-	});
+  describe('markCellNotRendered', () => {
+    it('should have a type of "MARK_CELL_NOT_RENDERED"', () => {
+      const expected = {
+        type: 'MARK_CELL_NOT_RENDERED',
+      };
 
-	describe('changeMode', () => {
+      const results = actions.markCellNotRendered();
 
-		it('should return the default value', () => {
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const expected = {
-				type: 'CHANGE_MODE',
-				mode: undefined
-			};
+  describe('insertCell', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'INSERT_CELL',
+        cellType: undefined,
+        direction: undefined,
+      };
 
-			const results = actions.changeMode();
+      const results = actions.insertCell();
 
-			expect(results).toEqual(expected)
-		})
+      expect(results).toEqual(expected)
+    })
 
-		it('should have a type of "CHANGE_MODE"', () => {
+    it('should have a type of "INSERT_CELL"', () => {
+      const cellType = 'Column';
+      const direction = 'Right'
 
-			const status = 'new mode';
+      const expected = {
+        type: 'INSERT_CELL',
+        cellType,
+        direction,
+      };
 
-			const expected = {
-				type: 'CHANGE_MODE',
-				mode: status
-			};
+      const results = actions.insertCell(cellType, direction);
 
-			const results = actions.changeMode(status);
+      expect(results).toEqual(expected)
+    });
+  });
 
-			expect(results).toEqual(expected)
-		});
+  describe('addCell', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'ADD_CELL',
+        cellType: undefined,
+      };
 
-	});
+      const results = actions.addCell();
 
-	describe('setViewMode', () => {
+      expect(results).toEqual(expected)
+    })
 
-		it('should return the default value', () => {
+    it('should have a type of "ADD_CELL"', () => {
+      const cellType = 'Column';
 
-			const expected = {
-				type: 'SET_VIEW_MODE',
-				viewMode: undefined
-			};
+      const expected = {
+        type: 'ADD_CELL',
+        cellType,
+      };
 
-			const results = actions.setViewMode();
+      const results = actions.addCell(cellType);
 
-			expect(results).toEqual(expected)
-		})
+      expect(results).toEqual(expected)
+    });
+  });
 
-		it('should have a type of "SET_VIEW_MODE"', () => {
+  describe('deleteCell', () => {
+    it('should have a type of "DELETE_CELL"', () => {
+      const expected = {
+        type: 'DELETE_CELL',
+      };
 
-			const status = 'new mode';
+      const results = actions.deleteCell();
 
-			const expected = {
-				type: 'SET_VIEW_MODE',
-				viewMode: status
-			};
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const results = actions.setViewMode(status);
+  describe('toggleEditorLink', () => {
+    it('should have a type of "TOGGLE_EDITOR_LINK"', () => {
+      const expected = {
+        type: 'TOGGLE_EDITOR_LINK',
+      };
 
-			expect(results).toEqual(expected)
-		});
+      const results = actions.toggleEditorLink();
 
-	});
+      expect(results).toEqual(expected)
+    });
+  });
 
-	describe('updateInputContent', () => {
+  describe('changeSidePaneMode', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'CHANGE_SIDE_PANE_MODE',
+        mode: undefined,
+      };
 
-		it('should return the default value', () => {
+      const results = actions.changeSidePaneMode();
 
-			const expected = {
-				type: 'UPDATE_INPUT_CONTENT',
-				content: undefined
-			};
+      expect(results).toEqual(expected)
+    })
 
-			const results = actions.updateInputContent();
+    it('should have a type of "CHANGE_SIDE_PANE_MODE"', () => {
+      const mode = 'selected';
 
-			expect(results).toEqual(expected)
-		})
+      const expected = {
+        type: 'CHANGE_SIDE_PANE_MODE',
+        mode,
+      };
 
-		it('should have a type of "UPDATE_INPUT_CONTENT"', () => {
+      const results = actions.changeSidePaneMode(mode);
 
-			const text = 'content text';
+      expect(results).toEqual(expected)
+    });
+  });
 
-			const expected = {
-				type: 'UPDATE_INPUT_CONTENT',
-				content: text
-			};
+  describe('changeSidePaneWidth', () => {
+    it('should return the default value', () => {
+      const expected = {
+        type: 'CHANGE_SIDE_PANE_WIDTH',
+        widthShift: undefined,
+      };
 
-			const results = actions.updateInputContent(text);
+      const results = actions.changeSidePaneWidth();
 
-			expect(results).toEqual(expected)
-		});
+      expect(results).toEqual(expected)
+    })
 
-	});
+    it('should have a type of "CHANGE_SIDE_PANE_WIDTH"', () => {
+      const widthShift = '10px';
 
-	describe('updateCellProperties', () => {
+      const expected = {
+        type: 'CHANGE_SIDE_PANE_WIDTH',
+        widthShift,
+      };
 
-		it('should return the default value', () => {
+      const results = actions.changeSidePaneWidth(widthShift);
 
-			const expected = {
-				type: 'UPDATE_CELL_PROPERTIES',
-				cellId: undefined,
-				updatedProperties: undefined
-			};
-
-			const results = actions.updateCellProperties();
-
-			expect(results).toEqual(expected)
-		})
-
-		it('should have a type of "UPDATE_INPUT_CONTENT"', () => {
-
-			const cellId = 1;
-			const updatedProperties = []
-
-			const expected = {
-				type: 'UPDATE_CELL_PROPERTIES',
-				cellId,
-				updatedProperties,
-			};
-
-			const results = actions.updateCellProperties(cellId, updatedProperties);
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('markCellNotRendered', () => {
-
-
-		it('should have a type of "MARK_CELL_NOT_RENDERED"', () => {
-
-
-			const expected = {
-				type: 'MARK_CELL_NOT_RENDERED',
-			};
-
-			const results = actions.markCellNotRendered();
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('insertCell', () => {
-
-		it('should return the default value', () => {
-
-			const expected = {
-				type: 'INSERT_CELL',
-				cellType: undefined,
-				direction: undefined
-			};
-
-			const results = actions.insertCell();
-
-			expect(results).toEqual(expected)
-		})
-
-		it('should have a type of "INSERT_CELL"', () => {
-
-			const cellType = 'Column';
-			const direction = 'Right'
-
-			const expected = {
-				type: 'INSERT_CELL',
-				cellType,
-				direction,
-			};
-
-			const results = actions.insertCell(cellType, direction);
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('addCell', () => {
-
-		it('should return the default value', () => {
-
-			const expected = {
-				type: 'ADD_CELL',
-				cellType: undefined
-			};
-
-			const results = actions.addCell();
-
-			expect(results).toEqual(expected)
-		})
-
-		it('should have a type of "ADD_CELL"', () => {
-
-			const cellType = 'Column';
-
-			const expected = {
-				type: 'ADD_CELL',
-				cellType,
-			};
-
-			const results = actions.addCell(cellType);
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('deleteCell', () => {
-
-		it('should have a type of "DELETE_CELL"', () => {
-
-
-			const expected = {
-				type: 'DELETE_CELL',
-			};
-
-			const results = actions.deleteCell();
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('toggleEditorLink', () => {
-
-		it('should have a type of "TOGGLE_EDITOR_LINK"', () => {
-
-
-			const expected = {
-				type: 'TOGGLE_EDITOR_LINK',
-			};
-
-			const results = actions.toggleEditorLink();
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('changeSidePaneMode', () => {
-
-		it('should return the default value', () => {
-
-			const expected = {
-				type: 'CHANGE_SIDE_PANE_MODE',
-				mode: undefined
-			};
-
-			const results = actions.changeSidePaneMode();
-
-			expect(results).toEqual(expected)
-		})
-
-		it('should have a type of "CHANGE_SIDE_PANE_MODE"', () => {
-
-			const mode = 'selected';
-
-			const expected = {
-				type: 'CHANGE_SIDE_PANE_MODE',
-				mode,
-			};
-
-			const results = actions.changeSidePaneMode(mode);
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
-	describe('changeSidePaneWidth', () => {
-
-		it('should return the default value', () => {
-
-			const expected = {
-				type: 'CHANGE_SIDE_PANE_WIDTH',
-				widthShift: undefined
-			};
-
-			const results = actions.changeSidePaneWidth();
-
-			expect(results).toEqual(expected)
-		})
-
-		it('should have a type of "CHANGE_SIDE_PANE_WIDTH"', () => {
-
-			const widthShift = '10px';
-
-			const expected = {
-				type: 'CHANGE_SIDE_PANE_WIDTH',
-				widthShift,
-			};
-
-			const results = actions.changeSidePaneWidth(widthShift);
-
-			expect(results).toEqual(expected)
-		});
-
-	});
-
+      expect(results).toEqual(expected)
+    });
+  });
 });
