886f9834580cd36b2a343b6a515ed1e1efb6c591
Quinlan Hill <quinhill@icloud.com>
2018-08-23 09:04:59 -0600

    Add more description to it block

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -2,7 +2,7 @@ import * as actions from '../actions';
 
 describe('Actions', () => {
   describe('exportNotebook', () => {
-    it('should return the default value', () => {
+    it('should return the default value if no argument is given', () => {
       const expected = {
         type: 'EXPORT_NOTEBOOK',
         exportAsReport: false,
