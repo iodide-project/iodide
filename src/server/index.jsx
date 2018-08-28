b7dcc09dde022140d610cd421abbff0fcdb3c36d
William Lachance <wrlach@gmail.com>
2018-08-28 11:11:14 -0400

    Unstyled notebook list on front page (#809)
    This expands the placeholder front page to include a primitive set of
    links to all the notebooks on the server. It's super ugly and obviously
    not scalable, but serves to illustrate the concept of rendering to
    a react-based template from django.

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Header from './components/header';
+import NotebookList from './components/notebook-list';
 
-const userInfo = window.userData;
+const { userInfo, notebookList } = window;
 
 render(
-  <div><Header userInfo={userInfo} /></div>,
+  <div>
+    <Header userInfo={userInfo} />
+    <NotebookList notebookList={notebookList} />
+  </div>,
   document.getElementById('page'),
 )
