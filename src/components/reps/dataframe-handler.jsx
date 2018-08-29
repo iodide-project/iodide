fd0fbb86300913f5bcc157b9d46fdb4428a2a2f4
Michael Droettboom <mdboom@gmail.com>
2018-08-29 11:28:31 -0400

    Use DevExtreme React Grid instead of react-table

--- src/components/reps/dataframe-handler.jsx
+++ src/components/reps/dataframe-handler.jsx
@@ -1,33 +1,62 @@
 import React from 'react'
-import ReactTable from 'react-table'
+import { Grid, VirtualTable, TableHeaderRow, Table } from '@devexpress/dx-react-grid-material-ui'
+import { DataTypeProvider, IntegratedSorting, SortingState } from '@devexpress/dx-react-grid'
 
 import { renderValue } from './value-renderer'
 import nb from '../../tools/nb'
+
+const ValueFormatter = ({ value }) => (
+  <div className="rep-base">
+    {renderValue(value, true)}
+  </div>
+)
+
+const ValueRendererProvider = props => (
+  <DataTypeProvider
+    formatterComponent={ValueFormatter}
+    {...props}
+  />
+)
+
+// Force heights to be based on contents, not the extremely spacious default
+const TableRow = props => (
+  <Table.Row
+    {... props}
+    style={{ height: 'auto' }}
+  />
+)
 
 export default {
   shouldHandle: (value, inContainer) => !inContainer && nb.isRowDf(value),
 
   render: (value) => {
     const columns = Object.keys(value[0])
-      .map(k => ({
-        Header: k,
-        accessor: k,
-        Cell: cell => renderValue(cell.value, true),
-      }))
+      .map(k => ({ name: k, title: k }))
     const dataSetInfo = `array of objects: ${value.length} rows, ${columns.length} columns`
-    const pageSize = value.length > 10 ? 10 : value.length
     return (
       <div className="dataframe-rep">
         <div className="data-set-info">{dataSetInfo}</div>
-        <ReactTable
-          data={value}
-          columns={columns}
-          showPaginationTop
-          showPaginationBottom={false}
-          pageSizeOptions={[5, 10, 25, 50, 100]}
-          minRows={0}
-          defaultPageSize={pageSize}
-        />
+        <Grid rows={value} columns={columns}>
+          <ValueRendererProvider
+            for={Object.keys(value[0])}
+          />
+          <SortingState
+            defaultSorting={[{
+              columnName: Object.keys(value[0])[0],
+              direction: 'asc',
+            }]}
+          />
+          <IntegratedSorting />
+          <VirtualTable
+            height={120}
+            estimatedRowHeight={24}
+            rowComponent={TableRow}
+          />
+          <TableHeaderRow
+            rowComponent={TableRow}
+            showSortingControls
+          />
+        </Grid>
       </div>
     )
   },
