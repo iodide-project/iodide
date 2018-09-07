6ce9464cb037248a283b002f7db573299eb20fa5
William Lachance <wlachance@mozilla.com>
2018-09-07 13:45:42 -0400

    Remove unused export-to-gist functionality
    This allows us to stop passing down the github access token to the
    client, which is/was a bit of a security no-no. If we ever choose
    to reimplement export-to-gist, we should do that on the server only.

--- src/components/menu/view-controls.jsx
+++ src/components/menu/view-controls.jsx
@@ -51,7 +51,7 @@ export class ViewControlsUnconnected extends React.Component {
 }
 
 export function mapStateToProps(state) {
-  const isAuthenticated = Boolean(state.userData.accessToken)
+  const isAuthenticated = Boolean(state.userData.name)
   return {
     isAuthenticated,
     name: state.userData.name,
