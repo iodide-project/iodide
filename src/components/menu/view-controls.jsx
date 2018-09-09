b1965d50ae94cce2afab72a893ac70f999bd7418
bcolloran <bcolloran@mozilla.com>
2018-09-09 13:58:48 -0700

    remove the side pane toggle buttons

--- src/components/menu/view-controls.jsx
+++ src/components/menu/view-controls.jsx
@@ -2,17 +2,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import InfoIcon from '@material-ui/icons/InfoOutline'
 
-import NotebookTaskButton from './notebook-task-button'
 import UserButton from './user-button'
 import ViewModeToggleButton from './view-mode-toggle-button'
 import LastSavedText from './last-saved-text'
-
-import tasks from '../../actions/task-definitions'
-import DeclaredVariableIcon from './declared-variable-icon'
-import ConsoleIcon from './console-icon'
-
 
 export class ViewControlsUnconnected extends React.Component {
   static propTypes = {
@@ -23,18 +16,6 @@ export class ViewControlsUnconnected extends React.Component {
     return (
       <div className="view-controls">
         <LastSavedText />
-
-        <NotebookTaskButton task={tasks.toggleAppInfoPane}>
-          <InfoIcon />
-        </NotebookTaskButton>
-
-        <NotebookTaskButton task={tasks.toggleDeclaredVariablesPane}>
-          <DeclaredVariableIcon />
-        </NotebookTaskButton>
-
-        <NotebookTaskButton task={tasks.toggleHistoryPane}>
-          <ConsoleIcon />
-        </NotebookTaskButton>
 
         {IODIDE_BUILD_TYPE === 'server' && (
           <UserButton
