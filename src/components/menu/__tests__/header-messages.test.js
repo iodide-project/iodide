8cc6dd1a3111582a81262d121df005c847b9b6e9
Michael Droettboom <mdboom@gmail.com>
2018-09-17 16:53:03 -0400

    Add tests

---
+++ src/components/menu/__tests__/header-messages.test.js
@@ -0,0 +1,45 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import { HeaderMessagesUnconnected, mapStateToProps } from '../header-messages'
+
+describe('HeaderMessagesUnconnected React component', () => {
+ 
+})
+
+
+describe('CellContainer mapStateToProps', () => {
+  let state
+  let ownProps
+
+  beforeEach(() => {
+    state = {
+      viewMode: 'EXPLORE_VIEW',
+      userData: {
+        name: 'user'
+      },
+      notebookInfo: {
+        user_can_save: true
+      },
+    }
+    ownProps = { }
+  })
+
+  it('displays login message', () => {
+    state.userData.name = undefined
+    state.notebookInfo.user_can_save = false
+    expect(mapStateToProps(state, ownProps).message)
+      .toEqual('NEED_TO_LOGIN')
+  })
+
+  it('displays make a copy message', () => {
+    state.notebookInfo.user_can_save = false
+    expect(mapStateToProps(state, ownProps).message)
+      .toEqual('NEED_TO_MAKE_COPY')
+  })
+
+  it('displays nothing', () => {
+    expect(mapStateToProps(state, ownProps).message)
+      .toEqual(undefined)
+  })
+})
