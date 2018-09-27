696101fa2188a5f75b73646717505da4498cce35
William Lachance <wlachance@mozilla.com>
2018-09-27 16:13:41 -0400

    WIP

---
+++ server/files/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import File
+
+
+class FilesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = File
+        fields = ('id', 'notebook_id', 'filename', 'last_updated')
