fa85edc389daae994960080aa1d5f7bc46d13154
Michael Droettboom <mdboom@gmail.com>
2018-09-27 15:09:34 -0400

    Make strings expand when clicking on them

--- src/components/reps/string-handler.jsx
+++ src/components/reps/string-handler.jsx
@@ -5,34 +5,42 @@ const MAX_CHARS = 1000
 export default {
   shouldHandle: value => typeof (value) === 'string',
   render: (value, inCollection) => {
-    const maxChars = inCollection ? 10 : MAX_CHARS
-    if (value.length <= maxChars) {
-      return (
-        <React.Fragment>
-          <span className="string-rep string-rep-before-quote string-rep-after-quote">{value}</span>
-        </React.Fragment>
-      )
+    let extraClasses = ''
+    if (inCollection) {
+      extraClasses = 'string-rep-truncated'
     }
 
-    // In a collection, just truncate and add "…".
-    // Outside a collection, insert the "X characters ommitted..." chunk in the middle
-    if (inCollection) {
+    const toggle = (e) => {
+      const { classList } = e.currentTarget
+      if (classList.contains('string-rep-truncated')) {
+        classList.replace('string-rep-truncated', 'string-rep-expanded')
+      } else if (classList.contains('string-rep-expanded')) {
+        classList.replace('string-rep-expanded', 'string-rep-truncated')
+      }
+    }
+
+    if (value.length <= MAX_CHARS) {
       return (
         <React.Fragment>
-          <span className="string-rep string-rep-before-quote">{value.slice(0, maxChars)}</span>
-          <span className="string-rep-ellipsis string-rep-after-quote">…</span>
+          <span onClick={toggle} className={`string-rep string-rep-before-quote string-rep-after-quote ${extraClasses}`}>
+            {value}
+          </span>
         </React.Fragment>
       )
     }
 
     return (
       <React.Fragment>
-        <span className="string-rep string-rep-before-quote">{value.slice(0, maxChars / 2)}</span>
-        <span className="elements-omitted-info-rep">
-          {`… ${value.length - maxChars} chars …`}
-        </span>
-        <span className="string-rep string-rep-after-quote">
-          {value.slice(value.length - Math.round(maxChars / 2))}
+        <span className={extraClasses} onClick={e => toggle(e)}>
+          <span className="string-rep string-rep-before-quote">
+            {value.slice(0, MAX_CHARS / 2)}
+          </span>
+          <span className="elements-omitted-info-rep">
+            {`… ${value.length - MAX_CHARS} chars …`}
+          </span>
+          <span className="string-rep string-rep-after-quote">
+            {value.slice(value.length - Math.round(MAX_CHARS / 2))}
+          </span>
         </span>
       </React.Fragment>
     )
