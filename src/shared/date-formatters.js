fa96da0a6dbe9b41f0f9496adc634e01b94487e1
hamilton <hamilton.ulmer@gmail.com>
2018-10-09 18:57:36 -0700

    fixed the time formatting function for 'x hours ago' by reading timezone information

--- src/shared/date-formatters.js
+++ src/shared/date-formatters.js
@@ -2,5 +2,5 @@ import formatDistance from 'date-fns/formatDistance'
 import parse from 'date-fns/parse'
 
 export function formatServerDate(dateString) {
-  return `${formatDistance(parse(dateString.slice(0, 19), 'yyyy-MM-dd kk:ss:SS', new Date()), new Date())} ago`
+  return `${formatDistance(parse(dateString, 'yyyy-MM-dd kk:mm:ss.SSSSSSxxx', new Date()), new Date())} ago`
 }
