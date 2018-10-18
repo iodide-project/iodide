b99afe56404695d3dbd4cf3181efd93eaddad297
hamilton <hamilton.ulmer@gmail.com>
2018-10-18 15:01:36 -0700

    removed extraneous console.log

--- src/handle-initial-view-mode-evaluation.js
+++ src/handle-initial-view-mode-evaluation.js
@@ -3,7 +3,6 @@ import { evaluateAllCells } from './actions/actions'
 export default function handleInitialViewModeEvaluation(store) {
   const state = store.getState()
   if (state.viewMode === 'REPORT_VIEW') {
-    console.log('here we goooo', state)
     store.dispatch(evaluateAllCells())
   }
 }
