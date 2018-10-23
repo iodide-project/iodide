74dbd7890a033907b738f3c6ca3184cbe6e0e2a9
hamilton <hamilton.ulmer@gmail.com>
2018-10-23 16:22:19 -0700

    added a test case for file PUT

--- server/tests/test_file_api.py
+++ server/tests/test_file_api.py
@@ -29,3 +29,29 @@ def test_post_to_file_api(fake_user, client, test_notebook):
             'filename': 'my cool file.csv',
             'notebook_id': test_notebook.id
         }
+
+
+def test_put_to_file_api(fake_user, client, test_notebook, test_file):
+    client.force_authenticate(user=fake_user)
+    with tempfile.NamedTemporaryFile(mode='w+') as f:
+        f.write('new-information')
+        f.seek(0)
+        print(reverse('files-detail', kwargs={'pk': test_file.id}))
+        resp = client.put(reverse('files-detail', kwargs={'pk': test_file.id}), {
+            'metadata': json.dumps({
+                'filename': 'test-2.csv',
+                'notebook_id': test_notebook.id
+            }),
+            'file': open(f.name)
+        })
+        assert resp.status_code == 201
+        assert File.objects.count() == 1
+        updated_file = File.objects.get(id=test_file.id)
+        assert updated_file.content.tobytes() == b'new-information'
+        assert updated_file.content.tobytes() != test_file.content
+        assert resp.json() == {
+            'id': updated_file.id,
+            'last_updated': get_rest_framework_time_string(updated_file.last_updated),
+            'filename': 'test-2.csv',
+            'notebook_id': test_notebook.id
+        }
