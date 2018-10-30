5d23ace9ceb46f0e6c046c0ab652cb8d291a6efd
hamilton <hamilton.ulmer@gmail.com>
2018-10-30 12:11:25 -0700

    added set-kernel-state.test.js

---
+++ src/actions/__tests__/set-kernel-state.test.js
@@ -0,0 +1,23 @@
+import { store } from '../../store'
+import { setKernelState, resetNotebook } from '../actions'
+import { SchemaValidationError } from '../../reducers/create-validated-reducer'
+import { mirroredStateProperties } from '../../state-schemas/mirrored-state-schema'
+
+describe('setKernelState', () => {
+  beforeEach(() => {
+    store.dispatch(resetNotebook())
+  })
+  it('createValidatedReducer should throw an error if we pass an action that inserts an invalid state value', () => {
+    // note that change mode must be a string
+    expect(() => store.dispatch(setKernelState('fake state')))
+      .toThrowError(SchemaValidationError)
+    expect(() => store.dispatch(setKernelState(12342323)))
+      .toThrowError(SchemaValidationError)
+  })
+  it('correctly sets the kernel state', () => {
+    mirroredStateProperties.kernelState.enum.forEach((kernelState) => {
+      store.dispatch(setKernelState(kernelState))
+      expect(store.getState().kernelState).toBe(kernelState)
+    })
+  })
+})
