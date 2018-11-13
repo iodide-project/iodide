d63f16792866cf439fcbad672f78013063892672
William Lachance <wlachance@mozilla.com>
2018-11-13 16:20:53 -0500

    Very basic google analytics support
    If you add a GA_TRACKING_ID variable in settings, it will add the
    google analytics tracking code to each page.

---
+++ server/context_processors.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.template.loader import render_to_string
+
+
+def google_analytics(request):
+    """
+    Returns analytics code.
+    """
+    if settings.GA_TRACKING_ID:
+        return {
+            'google_analytics': render_to_string(
+                'ga.html',
+                {'GA_TRACKING_ID': settings.GA_TRACKING_ID})
+        }
+    else:
+        return {'google_analytics': ''}
