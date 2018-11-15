7da34225788e3693c2ad0054c7dfad2710e22d35
bcolloran <bcolloran@mozilla.com>
2018-11-15 14:40:53 -0800

    clean out some menu items that were still pointing at deprecated tasks (#1182)

--- src/components/menu/editor-mode-controls.jsx
+++ src/components/menu/editor-mode-controls.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import AddButton from '@material-ui/icons/Add'
-import UpArrow from '@material-ui/icons/ArrowUpward'
-import DownArrow from '@material-ui/icons/ArrowDownward'
 import PlayButton from '@material-ui/icons/PlayArrow'
 import FastForward from '@material-ui/icons/FastForward'
 
@@ -16,15 +13,7 @@ export default class EditorModeControls extends React.Component {
     return (
       <div className="editor-mode-controls" >
         <EditorToolbarMenu />
-        <NotebookTaskButton task={tasks.addCellBelow}>
-          <AddButton />
-        </NotebookTaskButton>
-        <NotebookTaskButton task={tasks.moveCellUp}>
-          <UpArrow />
-        </NotebookTaskButton>
-        <NotebookTaskButton task={tasks.moveCellDown}>
-          <DownArrow />
-        </NotebookTaskButton>
+
         <NotebookTaskButton task={tasks.evaluateCell}>
           <PlayButton />
         </NotebookTaskButton>
