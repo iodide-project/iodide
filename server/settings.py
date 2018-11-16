0c82af2c99c5c537d146e6a8e5f7956ea962f772
William Lachance <wlachance@mozilla.com>
2018-11-16 14:38:51 -0500

    Move whitenoise middleware after security middleware
    Per recommendation in whitenoise documentation:
    
    http://whitenoise.evans.io/en/stable/django.html#enable-whitenoise

--- server/settings.py
+++ server/settings.py
@@ -79,8 +79,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
