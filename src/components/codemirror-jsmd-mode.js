896a16b04c73663db19f83890b6c3ae74c455b91
bcolloran <bcolloran@mozilla.com>
2018-11-26 14:59:41 -0800

    tweaks to clean up CM jsmd mode. fixes #1177

--- src/components/codemirror-jsmd-mode.js
+++ src/components/codemirror-jsmd-mode.js
@@ -2,52 +2,68 @@
 /* need to disable no-param-reassign b/c codemirror require mutating state */
 import CodeMirror from 'codemirror'
 
-CodeMirror.defineMode('jsmd', () => {
-  const jsMode = CodeMirror.getMode({}, { name: 'javascript' })
-  const mdMode = CodeMirror.getMode({}, { name: 'markdown' })
-  const plainMode = CodeMirror.getMode({}, { name: 'text/plain' })
-  const fetchMode = CodeMirror.getMode({}, { name: 'fetch' })
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/htmlmixed/htmlmixed'
 
-  return {
-    startState: () => ({
-      localMode: plainMode,
-      localState: null,
-    }),
-    token: (stream, state) => {
-      let thisToken
-      if (stream.sol() && stream.match(/%%\s*js.*/, true)) {
-        thisToken = 'line-background-cm-jsmd-delim-line'
-        state.localMode = jsMode
-        state.localState = jsMode.startState()
-      } else if (stream.sol() && stream.match(/%%\s*md.*/, true)) {
-        thisToken = 'line-background-cm-jsmd-delim-line'
-        state.localMode = mdMode
-        state.localState = mdMode.startState()
-      } else if (stream.sol() && stream.match(/%%\s*fetch.*/, true)) {
-        thisToken = 'line-background-cm-jsmd-delim-line'
-        state.localMode = fetchMode
-        state.localState = fetchMode.startState()
-      } else if (stream.sol() && stream.match(/%%.*/, true)) {
-        // NB: THIS ELSE IF HAS TO COME LAST !!!!!
-        // any '%%' without a known chunk type just
-        // delimits the block without changing mode
-        thisToken = 'line-background-cm-jsmd-delim-line'
-      } else {
-        thisToken = state.localMode.token(stream, state.localState)
+export const delimLineRegex = /^%%\s*(\w*)/
+
+export const innerModes = {
+  js: CodeMirror.getMode({}, { name: 'javascript' }),
+  md: CodeMirror.getMode({}, { name: 'markdown' }),
+  css: CodeMirror.getMode({}, { name: 'css' }),
+  raw: CodeMirror.getMode({}, { name: 'text/plain' }),
+  fetch: CodeMirror.getMode({}, { name: 'fetch' }),
+}
+innerModes.raw.startState = () => null
+
+CodeMirror.defineMode('jsmd', () => ({
+  startState: () => ({
+    localMode: innerModes.raw,
+    localState: null,
+  }),
+  token: (stream, state) => {
+    let thisToken
+    let delimLineMatch
+    let chunkType
+    if (stream.sol() && stream.peek() === '%') {
+      const match = stream.match(delimLineRegex, false)
+      if (match !== null) { [delimLineMatch, chunkType] = match }
+    }
+    if (delimLineMatch) {
+      // if this is a delim line, set the token and advance the stream
+      thisToken = 'line-background-cm-jsmd-delim-line'
+      stream.skipToEnd()
+      // handle the chunkType
+      if (chunkType !== '') {
+        // only change the inner mode if a new chunk type is delared
+        if (Object.keys(innerModes).includes(chunkType)) {
+          // if the chunkType is known, use the corresponding mode
+          state.localMode = innerModes[chunkType]
+        } else {
+          // if the chunk type is not know, use the raw text mode
+          state.localMode = innerModes.raw
+        }
       }
-      return thisToken
-    },
+      // reset the state of the localmode
+      state.localState = state.localMode.startState()
+    } else {
+      thisToken = state.localMode.token(stream, state.localState)
+    }
+    return thisToken
+  },
 
-    indent: (state, textAfter, line) => {
-      if (state.localMode.indent) {
-        const indent = state.localMode.indent(state.localState, textAfter, line)
-        console.log('indent:', indent)
-        return indent
-      }
-      console.log('no local indent available')
-      return CodeMirror.Pass
-    },
+  indent: (state, textAfter, line) => {
+    if (state.localMode.indent) {
+      const indent = state.localMode.indent(state.localState, textAfter, line)
+      console.log('indent:', indent)
+      return indent
+    }
+    console.log('no local indent available')
+    return CodeMirror.Pass
+  },
 
-    innerMode: state => ({ state: state.localState, mode: state.localMode }),
-  }
-})
+  innerMode: state => ({ state: state.localState, mode: state.localMode }),
+}))
+
