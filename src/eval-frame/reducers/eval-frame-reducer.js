82028e0a32ea474a92525144a7f89441ad306648
hamilton <hamilton.ulmer@gmail.com>
2018-11-27 12:48:56 -0800

    fixed lint error

--- src/eval-frame/reducers/eval-frame-reducer.js
+++ src/eval-frame/reducers/eval-frame-reducer.js
@@ -1,4 +1,4 @@
-import { newNotebook, newCell, newCellID } from '../eval-frame-state-prototypes'
+import { newNotebook, newCellID } from '../eval-frame-state-prototypes'
 
 function clearUserDefinedVars(userDefinedVarNames) {
   // remove user defined variables when loading/importing a new/saved NB
