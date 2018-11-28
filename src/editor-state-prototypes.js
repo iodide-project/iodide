737f5d9d0e436880ee79cddbf1301dcbf3170fd1
bcolloran <bcolloran@mozilla.com>
2018-11-28 15:49:29 -0800

    removes jsmd tools and some cell stuff, related clean up around that

--- src/editor-state-prototypes.js
+++ src/editor-state-prototypes.js
@@ -1,34 +1,14 @@
-import {
-  // newEditorCell,
-  // newEditorNotebook,
-  editorCellSchema,
-  editorStateSchema,
-} from './state-schemas/editor-only-state-schemas'
+import { editorStateSchema } from './state-schemas/editor-only-state-schemas'
 
-import {
-  newCellFromSchema,
-  newNotebookFromSchema,
-} from './state-schemas/state-prototype-from-schema'
+import { newNotebookFromSchema } from './state-schemas/state-prototype-from-schema'
 
 const stateSchema = editorStateSchema
-
-export const paneRatios = [0, 0.33, 0.5, 0.66, 1]
-
-function newCell(cellId, cellType = 'code', language = 'js') {
-  return newCellFromSchema(editorCellSchema, cellId, cellType, language)
-}
-
-function newCellID(cells) {
-  return Math.max(-1, ...cells.map(c => c.id)) + 1
-}
 
 function newNotebook() {
   return newNotebookFromSchema(editorStateSchema)
 }
 
 export {
-  newCell,
-  newCellID,
   newNotebook,
   stateSchema,
 }
