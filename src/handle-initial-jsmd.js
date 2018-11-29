665bc136002d46743e062bf5b4e8d03cdaa8ecf8
bcolloran <bcolloran@mozilla.com>
2018-11-29 15:31:31 -0800

    removing url loading options, adding tests

--- src/handle-initial-jsmd.js
+++ src/handle-initial-jsmd.js
@@ -1,36 +1,9 @@
-import { getStateFromJsmdQueryParams, getUrlParams, JSMD_QUERY_PARAMS } from './tools/query-param-tools'
-import { updateAppMessages, importInitialJsmd } from './actions/actions'
-import { jsmdParser } from './actions/jsmd-parser'
-import { getNotebookInfoFromDocument } from './tools/server-tools'
 
-export default async function handleInitialJsmd(store) {
-  let state = {}
-  let notebookImportedFromUrl = false
-  const urlParams = getUrlParams()
-  const jsmdURLPresent = Object.keys(urlParams).some(param => JSMD_QUERY_PARAMS.has(param))
-  const notebookInfo = getNotebookInfoFromDocument() || undefined
-  if (jsmdURLPresent && notebookInfo) {
-    const newState = await getStateFromJsmdQueryParams()
-    if (newState !== undefined) {
-      state = newState
-      notebookImportedFromUrl = true
-    }
-  } else {
-    // if there is no query string, attempt to parse jsmd from html
-    const jsmdElt = document.getElementById('jsmd')
-    if (jsmdElt &&
-        jsmdElt.innerHTML &&
-        jsmdElt.innerHTML.trim() !== '') {
-      const jsmd = jsmdElt.innerHTML
-      // state = stateFromJsmd(jsmd)
-      state.jsmd = jsmd
-      state.jsmdChunks = jsmdParser(jsmd)
-    }
-  }
-  if (state !== undefined) {
-    store.dispatch(importInitialJsmd(state))
-    if (notebookImportedFromUrl) {
-      store.dispatch(updateAppMessages({ message: 'Notebook imported from URL.' }))
-    }
+import { updateJsmdContent } from './actions/actions'
+
+export default function handleInitialJsmd(store) {
+  const jsmdElt = document.getElementById('jsmd')
+  if (jsmdElt && jsmdElt.innerHTML) {
+    store.dispatch(updateJsmdContent(jsmdElt.innerHTML))
   }
 }
--- src/tools/query-param-tools.js
+++ src/tools/query-param-tools.js
@@ -1,33 +1,5 @@
 import queryString from 'query-string'
 
-import { importIpynb } from './ipynb-import'
-
-async function loadJsmdFromNotebookUrl(url) {
-  let state
-  try {
-    const response = await fetch(url);
-    const notebookString = await response.text()
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(notebookString, 'text/html');
-    state.jsmd = doc.querySelector('#jsmd').innerHTML
-  } catch (err) {
-    console.error('failed to load notebook url', err);
-  }
-  return state
-}
-
-async function loadIpynbFromUrl(url) {
-  let state
-  try {
-    const response = await fetch(url)
-    const ipynbJson = await response.json()
-    state.jsmd = importIpynb(url, ipynbJson)
-  } catch (err) {
-    console.error('failed to load ipynb url', err)
-  }
-  return state
-}
-
 export function getUrlParams() {
   return queryString.parse(window.location.search)
 }
@@ -40,19 +12,4 @@ export function getStatePropsFromUrlParams() {
   const queryParams = getUrlParams()
   const report = queryParams.viewMode === 'report'
   return { viewMode: report ? 'REPORT_VIEW' : 'EXPLORE_VIEW' }
-}
-
-export const JSMD_QUERY_PARAMS = new Set(['url', 'gist', 'ipynb', 'jsmd'])
-
-export function getStateFromJsmdQueryParams() {
-  const queryParams = getUrlParams()
-  let state
-  if ({}.hasOwnProperty.call(queryParams, 'url')) {
-    state = loadJsmdFromNotebookUrl(queryParams.url)
-  } else if ({}.hasOwnProperty.call(queryParams, 'gist')) {
-    state = loadJsmdFromNotebookUrl(`https://gist.githubusercontent.com/${queryParams.gist}/raw/`)
-  } else if ({}.hasOwnProperty.call(queryParams, 'ipynb')) {
-    state = loadIpynbFromUrl(queryParams.ipynb);
-  }
-  return state
 }
