6a9d99b15ba0032eac80808f0f19ec93fd83105e
bcolloran <bcolloran@mozilla.com>
2018-12-07 15:02:04 -0500

    remove unused cases in eval frame action forwarder

--- src/reducers/eval-frame-action-forwarder.js
+++ src/reducers/eval-frame-action-forwarder.js
@@ -5,23 +5,13 @@ import { postActionToEvalFrame } from '../port-to-eval-frame'
 // this function forwards certain white-listed actions to the eval-frame.
 // If the eval frame is not ready, it automatically enqueues them for later sending.
 
+// FIXME the evalFrameActionForwarder is needlessly heavy,
+//  but for now its message queue is required for run-all
+// work when starting in view mode
 
 const evalFrameActionForwarder = (state, action) => {
   switch (action.type) {
-    // notebook level actions
-    case 'SET_VIEW_MODE':
-    case 'CHANGE_SIDE_PANE_MODE':
-    case 'TOGGLE_EDITOR_LINK':
-    case 'UPDATE_APP_MESSAGES':
-    case 'CLEAR_VARIABLES':
-    case 'UPDATE_PANE_POSITIONS':
-    case 'TRIGGER_TEXT_EVAL_IN_FRAME':
-    case 'UPDATE_MARKDOWN_CHUNKS':
-    // special case: passes to eval frame without changing editor pane state,
-    // but by sending them as redux actions, they are automatically queued
-    // until the ports are open
-    case 'TRIGGER_CELL_EVAL_IN_FRAME':
-    case 'UPDATE_EVAL_FRAME_FROM_INITIAL_JSMD': {
+    case 'TRIGGER_TEXT_EVAL_IN_FRAME': {
       if (state.evalFrameReady) {
         // if the eval frame is ready, send the action on to the frame
         //  and return the identical state
