d0de454937f6c7bf2a24cb45b26644c548d7e878
hamilton <hamilton.ulmer@gmail.com>
2018-12-13 15:49:37 -0800

    added small enzyme test to make sure we're rendering the right status icon component

--- src/components/menu/__tests__/kernel-state.test.js
+++ src/components/menu/__tests__/kernel-state.test.js
@@ -1,18 +1,47 @@
-import { mapStateToProps } from '../kernel-state'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import CircularProgress from '@material-ui/core/CircularProgress'
+import PanoramaFishEye from '@material-ui/icons/PanoramaFishEye'
+import Error from '@material-ui/icons/Error'
+import ErrorOutline from '@material-ui/icons/ErrorOutline'
+
+import { KernelStateUnconnected, mapStateToProps } from '../kernel-state'
+
+const states = [
+  {
+    kernelState: 'KERNEL_BUSY', color: undefined, kernelText: 'Kernel Busy', statusIcon: CircularProgress,
+  },
+  {
+    kernelState: 'KERNEL_LOADING', color: undefined, kernelText: 'Kernel Loading', statusIcon: CircularProgress,
+  },
+  {
+    kernelState: 'KERNEL_IDLE', color: 'forestgreen', kernelText: 'Kernel Idle', statusIcon: PanoramaFishEye,
+  },
+  {
+    kernelState: 'KERNEL_ERROR', color: 'gray', kernelText: 'Kernel Error', statusIcon: ErrorOutline,
+  },
+  {
+    kernelState: 'KERNEL_LOAD_ERROR', color: 'red', kernelText: 'Kernel Didn\'t Load', statusIcon: Error,
+  },
+]
+
+describe('KernelStateUnconnected', () => {
+  it('properly renders the status icon component for the correct kernel state', () => {
+    states.forEach((st) => {
+      const props = { kernelText: st.kernelText, color: st.color }
+      const component = shallow(<KernelStateUnconnected {...props} />)
+      expect(component.find(st.statusIcon).length).toBe(1)
+    })
+  })
+})
 
 describe('mapStateToProps', () => {
-  const states = [
-    { kernelState: 'KERNEL_BUSY', expectedColor: undefined, expectedText: 'Kernel Busy' },
-    { kernelState: 'KERNEL_LOADING', expectedColor: undefined, expectedText: 'Kernel Loading' },
-    { kernelState: 'KERNEL_IDLE', expectedColor: 'forestgreen', expectedText: 'Kernel Idle' },
-    { kernelState: 'KERNEL_ERROR', expectedColor: 'gray', expectedText: 'Kernel Error' },
-    { kernelState: 'KERNEL_LOAD_ERROR', expectedColor: 'red', expectedText: 'Kernel Didn\'t Load' },
-  ]
   it('maps the kernelState to color / tooltip text', () => {
     states.forEach((st) => {
       const props = mapStateToProps(st)
-      expect(props.color).toBe(st.expectedColor)
-      expect(props.kernelText).toBe(st.expectedText)
+      expect(props.color).toBe(st.color)
+      expect(props.kernelText).toBe(st.kernelText)
     })
   })
 })
