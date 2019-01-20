f26e9df987606080a85299fc3706aeec0b728967
bcolloran <bcolloran@mozilla.com>
2019-01-20 00:24:04 -0800

    remove useless code (unneeded prop)

--- src/eval-frame/components/panes/declared-variable.jsx
+++ src/eval-frame/components/panes/declared-variable.jsx
@@ -9,12 +9,11 @@ export class DeclaredVariable extends React.Component {
     varName: PropTypes.string
   };
   render() {
-    const r = true;
     return (
       <div className="declared-variable">
         <div className="declared-variable-name">{this.props.varName} = </div>
         <div className="declared-variable-value">
-          <ValueRenderer render={r} valueToRender={this.props.value} />
+          <ValueRenderer valueToRender={this.props.value} />
         </div>
       </div>
     );
