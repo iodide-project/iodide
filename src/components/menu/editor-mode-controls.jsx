3edde3d4fa371383d67e34047173fe12142be495
hamilton <hamilton.ulmer@gmail.com>
2019-01-22 12:49:44 -0800

    removed unneeded container element

--- src/components/menu/editor-mode-controls.jsx
+++ src/components/menu/editor-mode-controls.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "react-emotion";
 
 import PlayButton from "@material-ui/icons/PlayArrow";
 import FastForward from "@material-ui/icons/FastForward";
@@ -9,16 +8,10 @@ import NotebookTaskButton from "./notebook-task-button";
 
 import tasks from "../../actions/task-definitions";
 
-// the min-width of 300px keeps the title bar centered.
-// this should be fixed by someone so the
-// title bar stays dead-center, but the left and right
-// containers take up the same amount of space.
-const EditorModeControlsContainer = styled("div")``;
-
 export default class EditorModeControls extends React.Component {
   render() {
     return (
-      <EditorModeControlsContainer>
+      <React.Fragment>
         <EditorToolbarMenu />
         <NotebookTaskButton task={tasks.evaluateChunkAndSelectBelow}>
           <PlayButton />
@@ -26,7 +19,7 @@ export default class EditorModeControls extends React.Component {
         <NotebookTaskButton task={tasks.evaluateNotebook}>
           <FastForward />
         </NotebookTaskButton>
-      </EditorModeControlsContainer>
+      </React.Fragment>
     );
   }
 }
