8005b01772c279cd48aed3de4fbcd0b3213604d9
hamilton <hamilton.ulmer@gmail.com>
2019-01-22 13:10:53 -0800

    simplified footer

--- src/server/components/footer.jsx
+++ src/server/components/footer.jsx
@@ -1,15 +1,8 @@
 /* global IODIDE_PUBLIC */
 import React from "react";
 import styled from "react-emotion";
-import LogoMark from "../../shared/components/logo/logo-mark";
 
 const FooterContainer = styled("footer")`
-  margin: auto;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-column-gap: 15px;
-  justify-items: center;
-  align-items: top;
   padding-top: 15px;
   padding-bottom: 15px;
   font-size: 0.815rem;
@@ -31,22 +24,32 @@ const FooterDiv = styled("div")`
   }
 `;
 
-const FooterLogoContainer = styled("div")`
-  opacity: 0.2;
-  svg {
-    width: 80px;
-  }
-`;
-
 export default ({ showIcon = true }) => (
   <FooterContainer showIcon={showIcon}>
     <FooterDiv>
       <p>
         iodide is brought to you by <a href="https://mozilla.org">Mozilla</a>.
       </p>
+      {IODIDE_PUBLIC && (
+        <p>
+          Content available under the terms of the&nbsp;
+          <a
+            href="https://creativecommons.org/licenses/by-sa/3.0/deed.en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Creative Commons Attribution-ShareAlike 3.0 Unported license
+          </a>
+          .
+        </p>
+      )}
       {// only display terms of service on an official mozilla installation
       IODIDE_PUBLIC && (
         <ul>
+          <li>Extremely alpha software</li>
+          <li>
+            <a href="https://github.com/iodide-project/iodide">Contribute</a>
+          </li>
           <li>
             <a href="https://www.mozilla.org/about/legal/terms/mozilla">
               Terms
@@ -64,32 +67,6 @@ export default ({ showIcon = true }) => (
       )}
       <p>
         <small>© 2018 Mozilla and other contributors</small>.
-      </p>
-    </FooterDiv>
-    {showIcon ? (
-      <FooterLogoContainer>
-        <LogoMark />
-      </FooterLogoContainer>
-    ) : (
-      <div />
-    )}
-    <FooterDiv>
-      {IODIDE_PUBLIC && (
-        <p>
-          Content available under the terms of the&nbsp;
-          <a
-            href="https://creativecommons.org/licenses/by-sa/3.0/deed.en"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Creative Commons Attribution-ShareAlike 3.0 Unported license
-          </a>
-          .
-        </p>
-      )}
-      <p>
-        Extremely alpha software -{" "}
-        <a href="https://github.com/iodide-project/iodide">contribute</a>.
       </p>
     </FooterDiv>
   </FooterContainer>
