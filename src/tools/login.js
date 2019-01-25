867da14896d3f0a59cf17d0c0b155bc2fd5855c4
William Lachance <wlachance@mozilla.com>
2019-01-25 16:20:39 -0500

    Simplify login code
    There was quite a bit of state and business logic that was unused
    here, and could be eliminated.

--- src/tools/login.js
+++ src/tools/login.js
@@ -1,4 +1,10 @@
-export function loginToServer() {
+export function loginToServer(successCallback) {
+  // set up a callback on the window object that the
+  // child window can call
+  window.loginSuccess = userData => {
+    successCallback(userData);
+  };
+
   const url = "/oauth/login/github";
   const name = "github_login";
   const specs = "width=500,height=600";
@@ -9,6 +15,7 @@ export function loginToServer() {
 export function logoutFromServer(successCallback, errorCallback, dispatch) {
   fetch("/logout/").then(response => {
     if (response.ok) {
+      successCallback(dispatch);
       successCallback(dispatch);
     } else {
       errorCallback(response, dispatch);
