92e7b05ee89468188e17b8e4c314939cecbfa91b
hamilton <hamilton.ulmer@gmail.com>
2019-02-06 10:14:00 -0800

    removed unused ConsoleBody

--- src/eval-frame/components/panes/console/console-body.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "react-emotion";
-import THEME from "../../../../shared/theme";
-
-export default styled("div")`
-  min-height: ${THEME.client.console.lineHeightPx}px;
-  display: flex;
-  align-items: center;
-`;
