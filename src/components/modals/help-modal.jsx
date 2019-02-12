c3cdc9979ce49aaa123126f4b17be8b781c3b002
William Lachance <wlachance@mozilla.com>
2019-02-12 13:52:17 -0500

    Refactor modal-container into its own file

--- src/components/modals/help-modal.jsx
+++ src/components/modals/help-modal.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "react-emotion";
 
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
+import { ModalContainer } from "./modal-container";
 import HelpModalContent from "./help-modal-content";
 
 import KeyboardShortcutList from "./keyboard-shortcut-list";
@@ -15,24 +15,6 @@ import FeaturedNotebooks from "../../shared/components/featured-notebooks";
 import tasks from "../../actions/task-definitions";
 
 import THEME from "../../shared/theme";
-
-const ModalContainer = styled("div")`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  height: 80%;
-  width: 60%;
-  min-width: 700px;
-  background: white;
-  box-shadow: 0px 3px 20px rgba(0, 0, 0, 0.5);
-  flex-direction: column;
-  display: flex;
-
-  @media (max-width: 1400px) {
-    width: 80%;
-  }
-`;
 
 const MoreResources = () => (
   <HelpModalContent>
---
+++ src/components/modals/modal-container.js
@@ -0,0 +1,25 @@
+import styled from "react-emotion";
+
+export const ModalContainer = styled("div")`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 80%;
+  width: 60%;
+  min-width: 700px;
+  background: white;
+  box-shadow: 0px 3px 20px rgba(0, 0, 0, 0.5);
+  flex-direction: column;
+  display: flex;
+
+  @media (max-width: 1400px) {
+    width: 80%;
+  }
+`;
+
+export const ModalContentContainer = styled("div")`
+  overflow: auto;
+  padding: 20px;
+  height: 100%;
+`;
