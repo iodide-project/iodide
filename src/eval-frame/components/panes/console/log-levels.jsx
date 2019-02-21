5c73bf7f5ef528ae8d0f8f874c741e87a2a65de7
hamilton <hamilton.ulmer@gmail.com>
2019-02-21 15:33:32 -0800

    changed BaseIcon to accept some styles

--- src/eval-frame/components/panes/console/log-levels.jsx
+++ src/eval-frame/components/panes/console/log-levels.jsx
@@ -6,10 +6,10 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 import BaseIcon from "./base-icon";
 
-const Error = BaseIcon(ErrorIcon);
-const Warning = BaseIcon(WarningIcon);
-const Info = BaseIcon(InfoIcon);
-const ArrowBack = BaseIcon(ArrowBackIcon);
+const Error = BaseIcon(ErrorIcon, { color: "rgb(215,0,34)" });
+const Warning = BaseIcon(WarningIcon, { color: "rgb(190,155,0)" });
+const Info = BaseIcon(InfoIcon, { opacity: 0.5 });
+const ArrowBack = BaseIcon(ArrowBackIcon, { opacity: 0.5 });
 
 const levels = {
   LOG: {
@@ -19,17 +19,17 @@ const levels = {
   },
   INFO: {
     backgroundColor: "white",
-    icon: <Info style={{ opacity: 0.5 }} />,
+    icon: <Info />,
     textColor: "rgba(0,0,0,.8)"
   },
   WARN: {
     backgroundColor: "rgb(255,251,214)",
-    icon: <Warning style={{ color: "rgb(190,155,0)" }} />,
+    icon: <Warning />,
     textColor: "rgb(131, 81, 15)"
   },
   ERROR: {
     backgroundColor: "rgb(253,244,245)",
-    icon: <Error style={{ color: "rgb(215,0,34)" }} />,
+    icon: <Error />,
     textColor: "rgb(164, 0, 15)"
   },
   OUTPUT: {
