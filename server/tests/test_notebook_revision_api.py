06b2c5f0af295a88b0123f217c96626d2e71512e
William Lachance <wlachance@mozilla.com>
2019-03-04 15:10:47 -0500

    Test server-side response when creating notebook revision

--- server/tests/test_notebook_revision_api.py
+++ server/tests/test_notebook_revision_api.py
@@ -136,9 +136,9 @@ def test_create_notebook_revision(fake_user, test_notebook, client):
     )
     assert resp.status_code == 201
     assert NotebookRevision.objects.count() == 2
-    notebook_revision = NotebookRevision.objects.first()
-    assert notebook_revision.title == post_blob["title"]
-    assert notebook_revision.content == post_blob["content"]
+    new_notebook_revision = NotebookRevision.objects.first()
+    assert new_notebook_revision.title == post_blob["title"]
+    assert new_notebook_revision.content == post_blob["content"]
 
     # make sure that the title gets updated too
     test_notebook.refresh_from_db()
