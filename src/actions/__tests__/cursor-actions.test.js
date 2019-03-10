8d1ae53299560d6559a410f8ce1abdfc1e4d7139
bcolloran <bcolloran@mozilla.com>
2019-03-09 18:44:11 -0800

    update tests

--- src/actions/__tests__/cursor-actions.test.js
+++ src/actions/__tests__/cursor-actions.test.js
@@ -91,4 +91,90 @@ describe("moveCursorToNextChunk dispatches correct actions", () => {
     store.dispatch(moveCursorToNextChunk());
     expect(store.getActions()).toEqual(expectedActions);
   });
+
+  it("one selection, and one chunk", () => {
+    const [editorCursorLine, editorCursorChar] = [1, 2];
+
+    const store = mockStore({
+      editorCursorLine,
+      editorCursorChar,
+      jsmdChunks: [{ startLine: 0, endLine: 10 }],
+      editorSelections: [
+        { start: { line: 3, col: 2 }, end: { line: 5, col: 2 } }
+      ]
+    });
+
+    const expectedActions = [
+      {
+        type: "UPDATE_CURSOR",
+        editorCursorLine: 11,
+        editorCursorChar: 0,
+        editorCursorForceUpdate: true
+      }
+    ];
+
+    store.dispatch(moveCursorToNextChunk());
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("one selection across multiple chunks", () => {
+    const [editorCursorLine, editorCursorChar] = [12, 25];
+
+    const store = mockStore({
+      editorCursorLine,
+      editorCursorChar,
+      jsmdChunks: [
+        { startLine: 0, endLine: 10 },
+        { startLine: 11, endLine: 15 },
+        { startLine: 16, endLine: 20 },
+        { startLine: 21, endLine: 100 }
+      ],
+      editorSelections: [
+        { start: { line: 3, col: 2 }, end: { line: 17, col: 2 } }
+      ]
+    });
+
+    const expectedActions = [
+      {
+        type: "UPDATE_CURSOR",
+        editorCursorLine: 21,
+        editorCursorChar: 0,
+        editorCursorForceUpdate: true
+      }
+    ];
+
+    store.dispatch(moveCursorToNextChunk());
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it("multiple selections in multiple chunks", () => {
+    const [editorCursorLine, editorCursorChar] = [12, 25];
+
+    const store = mockStore({
+      editorCursorLine,
+      editorCursorChar,
+      jsmdChunks: [
+        { startLine: 0, endLine: 10 },
+        { startLine: 11, endLine: 15 },
+        { startLine: 16, endLine: 20 },
+        { startLine: 21, endLine: 100 }
+      ],
+      editorSelections: [
+        { start: { line: 3, col: 2 }, end: { line: 7, col: 2 } },
+        { start: { line: 13, col: 2 }, end: { line: 19, col: 2 } }
+      ]
+    });
+
+    const expectedActions = [
+      {
+        type: "UPDATE_CURSOR",
+        editorCursorLine: 21,
+        editorCursorChar: 0,
+        editorCursorForceUpdate: true
+      }
+    ];
+
+    store.dispatch(moveCursorToNextChunk());
+    expect(store.getActions()).toEqual(expectedActions);
+  });
 });
