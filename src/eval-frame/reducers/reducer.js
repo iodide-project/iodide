1f35e7c67a77ba6bb728e985b55c377d74e0d0d8
bcolloran <bcolloran@mozilla.com>
2019-03-17 02:26:03 -0700

    eval frame no longer needs reducer hack to forward actions

--- src/eval-frame/reducers/reducer.js
+++ src/eval-frame/reducers/reducer.js
@@ -1,29 +1,3 @@
-// /*  global IODIDE_BUILD_MODE */
-import { postActionToEditor } from "../port-to-editor";
-
-function reduceReducers(...reducers) {
-  return (previous, current) =>
-    reducers.reduce((p, r) => r(p, current), previous);
-}
-
-function replaceStateFromEditor(state, action) {
+export default function reducer(state, action) {
   return action.type === "REPLACE_STATE" ? action.state : state;
 }
-
-// this function forwards actions to the editor
-// FIXME THIS IS CRITICAL
-// for securty reasons, any actions forwarded must be whitelisted
-// and verified against schema on the editor side.
-const actionForwarder = (state, action) => {
-  // FIXME: this try.catch required to allow the eval frame to load properly --
-  // because we have circular dependencies in imports, postActionToEditor
-  // is not actually initialized when this is called the first time
-  try {
-    if (action.type !== "REPLACE_STATE") postActionToEditor(action);
-  } catch (error) {
-    console.log("EVAL FRAME ACTION POST TO EDITOR FAILED");
-  }
-  return state;
-};
-
-export default reduceReducers(replaceStateFromEditor, actionForwarder);
