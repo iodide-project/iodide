e4a0caeea3317c3e6d80b114211f01617cedb66a
William Lachance <wlachance@mozilla.com>
2019-03-18 17:02:30 -0400

    Add user administration functionality to /admin

---
+++ server/base/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser or request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_module_permission(self, request):
+        return request.user.is_superuser or request.user.is_staff
+
+    fieldsets = [
+        (
+            None,
+            {"fields": ["username", "can_create_on_behalf_of_others", "is_staff", "is_superuser"]},
+        )
+    ]
+
+
+admin.site.register(User, UserAdmin)
