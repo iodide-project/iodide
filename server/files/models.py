2752c94cb9fa6ad8299ccd063d5b2dd3ae51bb71
William Lachance <wlachance@mozilla.com>
2019-07-01 10:01:15 -0700

    WIP

--- server/files/models.py
+++ server/files/models.py
@@ -24,3 +24,52 @@ class File(models.Model):
         verbose_name = "File"
         verbose_name_plural = "Files"
         ordering = ("id",)
+
+
+class FileSource(models.Model):
+    """
+    Represents a source for files (an external URL)
+    """
+    notebook = models.ForeignKey(Notebook, on_delete=models.CASCADE)
+    file = models.OneToOneField(
+        File,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    # FIXME: add a validator for filename (for minimum length and maybe
+    # other things)
+    filename = models.CharField(max_length=MAX_FILENAME_LENGTH)
+    source = models.URLField()
+    update_interval = models.DurationField(null=True)
+
+    def __str__(self):  # pragma: no cover
+        return self.source
+
+    class Meta:
+        verbose_name = "File Source"
+        verbose_name_plural = "File Sources"
+        ordering = ("id",)
+        db_table = "file_source"
+
+
+class FileUpdateOperation(models.Model):
+    """
+    Represents a pending, ongoing, or completed operation to update a file
+    """
+    PENDING = 0
+    RUNNING = 1
+    COMPLETED = 2
+    OPERATION_STATUSES = ((PENDING, "pending"),
+                          (RUNNING, "running"),
+                          (COMPLETED, "completed"))
+
+    file_source = models.ForeignKey(FileSource, on_delete=models.CASCADE)
+    started = models.DateTimeField(auto_now_add=True)
+    ended = models.DateTimeField(null=True)
+    status = models.IntegerField(choices=OPERATION_STATUSES, default=PENDING)
+
+    def __str__(self):  # pragma: no cover
+        return '{} update ({})'.format(file_source, OPERATION_STATUSES[self.status][1])
+
+    class Meta:
+        db_table = "file_operation"
--- server/files/serializers.py
+++ server/files/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 
-from .models import File
+from ..notebooks.models import Notebook
+from .models import (File, FileSource)
 
 
 class FilesSerializer(serializers.ModelSerializer):
+    """
+    Gives a summary of file (not its content)
+    """
     class Meta:
         model = File
         fields = ("id", "notebook_id", "filename", "last_updated")
+
+
+class FileSourceSerializer(serializers.ModelSerializer):
+    """
+    All the properties of a file source, which can be used to retrieve or
+    update a file from a URL
+    """
+    notebook_id = serializers.PrimaryKeyRelatedField(
+        source="notebook", queryset=Notebook.objects.all())
+
+
+    class Meta:
+        model = FileSource
+        fields = ("id", "notebook_id", "file_id", "filename", "source", "update_interval")
