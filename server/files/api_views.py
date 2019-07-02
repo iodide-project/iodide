5983146fa2e7efef657a014b8a78c7c88f12872c
William Lachance <wlachance@mozilla.com>
2019-07-02 08:37:00 -0400

    fix other issues pointed out in review

--- server/files/api_views.py
+++ server/files/api_views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 
@@ -15,39 +16,40 @@ class FileViewSet(viewsets.ModelViewSet):
 
     http_method_names = ["post", "put", "delete"]
 
-    def perform_destroy(self, instance):
-        if instance.notebook.owner != self.request.user:
-            raise PermissionDenied
-        super().perform_destroy(instance)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.notebook.owner != request.user:
+             raise PermissionDenied
+        return super().destroy(request, *args, **kwargs)
 
     def create(self, request):
         metadata = json.loads(self.request.data["metadata"])
-        file =  self.request.data["file"]
+        file = self.request.data["file"]
 
-        notebook = Notebook.objects.get(id=metadata["notebook_id"])
+        notebook = get_object_or_404(Notebook, id=metadata["notebook_id"])
         if notebook.owner != self.request.user:
             raise PermissionDenied
 
-        f = File.objects.create(
+        file_obj = File.objects.create(
             notebook_id=notebook.id, filename=metadata["filename"], content=file.read()
         )
-        serializer = FilesSerializer(f)
-        return Response(serializer.data, status=201)
+        return Response(FilesSerializer(file_obj).data, status=201)
 
     def update(self, request, pk):
-        (metadata, file) = (json.loads(self.request.data["metadata"]), self.request.data["file"])
+        metadata = json.loads(self.request.data["metadata"])
+        file = self.request.data["file"]
 
-        notebook = Notebook.objects.get(id=metadata["notebook_id"])
+        notebook = get_object_or_404(Notebook, id=metadata["notebook_id"])
         if notebook.owner != self.request.user:
             raise PermissionDenied
 
-        file_to_update = File.objects.get(pk=pk)
+        file_obj_to_update = get_object_or_404(File, pk=pk)
         updated_filename = metadata["filename"].strip()
-        file_to_update.filename = updated_filename
+        file_obj_to_update.filename = updated_filename
         if file:
-            file_to_update.content = file.read()
-        file_to_update.save()
-        serializer = FilesSerializer(file_to_update)
-        return Response(serializer.data, status=201)
+            file_obj_to_update.content = file.read()
+        file_obj_to_update.save()
+
+        return Response(FilesSerializer(file_obj_to_update).data, status=201)
 
     queryset = File.objects.all()
