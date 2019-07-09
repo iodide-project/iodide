a1cfec53c5c0e638d896579e9ef3837f33234036
hamilton <hamilton.ulmer@gmail.com>
2019-07-09 13:53:56 -0700

    file modal now has tabs

--- src/editor/components/modals/file-modal/index.jsx
+++ src/editor/components/modals/file-modal/index.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 import { ModalContainer } from "../modal-container";
-import TitleBar from "../title-bar";
 import ManageFiles from "./manage-files";
-
 import OfflineFetcher from "../offline-fetcher/offline-fetcher";
+import THEME from "../../../../shared/theme";
 
-export default () => (
-  <ModalContainer tabIndex="-1">
-    <TitleBar title="Manage Files" />
-    <ManageFiles />
-    <OfflineFetcher />
-  </ModalContainer>
-);
+export default () => {
+  const [tab, setTab] = useState(0);
+  const changeTab = (evt, val) => setTab(val);
+  return (
+    <ModalContainer tabIndex="-1">
+      <AppBar
+        position="static"
+        style={{
+          background: THEME.clientModal.background
+        }}
+      >
+        <Tabs value={tab} onChange={changeTab}>
+          <Tab label="Manage Files" />
+          <Tab label="Manage File Sources" />
+        </Tabs>
+      </AppBar>
+      {tab === 0 && <ManageFiles />}
+      {tab === 1 && <OfflineFetcher />}
+    </ModalContainer>
+  );
+};
