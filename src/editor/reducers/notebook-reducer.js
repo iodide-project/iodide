d4ca72cacb23d77c007a1b566b04988894cbfd3f
Your Name <baylyd@email.arizona.edu>
2019-07-15 15:40:43 -0700

    changes to try to support newline char insert detection, its a plugin problem actually

--- src/editor/reducers/notebook-reducer.js
+++ src/editor/reducers/notebook-reducer.js
@@ -78,11 +78,19 @@ const notebookReducer = (state = newNotebook(), action) => {
       const { line, col, forceUpdate, text } = action;
       // insert text in state.iomd at correct position without mutating
       console.log(line, col, forceUpdate, text);
-      const iomd = state.iomd
-        .split("\n")
+      // first check that the number of lines is the same if not add new lines to bottom of iomd file
+      let lines = state.iomd.split("\n");
+      if (lines.length < line) {
+        lines = lines.concat(Array(line - lines.length + 1).fill("")); // equalize line number
+      }
+      const iomd = lines
         .map((iomdLine, i) => {
           // if its the correct line insert the new text in this place
           if (i === line) {
+            // if this line represents a new line from the editor
+            if (iomdLine === "") {
+              return text;
+            }
             return iomdLine.slice(0, col) + text + iomdLine.slice(col);
           }
           return iomdLine;
