6af37f6bf5bc14ef94359441687aed71bfa6b79b
bcolloran <bcolloran@mozilla.com>
2019-07-25 00:19:24 -0700

    add IODIDE_EVALUATION_RESULTS to initialVariables

--- src/eval-frame/initialize-user-variables.js
+++ src/eval-frame/initialize-user-variables.js
@@ -3,6 +3,7 @@ import { sendActionToEditor } from "./actions/editor-message-senders";
 const initialVariables = new Set(Object.keys(window)); // gives all global variables
 initialVariables.add("__core-js_shared__");
 initialVariables.add("__SECRET_EMOTION__");
+initialVariables.add("IODIDE_EVALUATION_RESULTS");
 
 export function getUserDefinedVariablesFromWindow() {
   return Object.keys(window).filter(g => !initialVariables.has(g));
