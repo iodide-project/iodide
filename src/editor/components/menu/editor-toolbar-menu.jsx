6fda4602acdd9a95e5f0af46d1850b365598459f
Ziyao Wei <ziyao.wei.wzy@gmail.com>
2019-08-08 09:32:33 -0400

    Disable history menu option for tryit notebook (fixes #2114) (#2125)

--- src/editor/components/menu/editor-toolbar-menu.jsx
+++ src/editor/components/menu/editor-toolbar-menu.jsx
@@ -5,21 +5,26 @@ import PropTypes from "prop-types";
 import NotebookIconMenu from "./icon-menu";
 import tasks from "../../user-tasks/task-definitions";
 import NotebookMenuItem from "./notebook-menu-item";
-import { connectionModeIsServer } from "../../tools/server-tools";
+import {
+  connectionModeIsServer,
+  notebookIsATrial
+} from "../../tools/server-tools";
 
 export class EditorToolbarMenuUnconnected extends React.Component {
   static propTypes = {
     isServer: PropTypes.bool.isRequired,
-    canUpload: PropTypes.bool
+    canUpload: PropTypes.bool,
+    isTrialNotebook: PropTypes.bool
   };
 
   render() {
     return (
       <NotebookIconMenu>
         {this.props.isServer && <NotebookMenuItem task={tasks.newNotebook} />}
-        {this.props.isServer && (
-          <NotebookMenuItem task={tasks.toggleHistoryModal} />
-        )}
+        <NotebookMenuItem
+          task={tasks.toggleHistoryModal}
+          disabled={this.props.isTrialNotebook}
+        />
         {this.props.canUpload && (
           <NotebookMenuItem task={tasks.toggleFileModal} />
         )}
@@ -35,6 +40,7 @@ export function mapStateToProps(state) {
 
   return {
     isServer,
+    isTrialNotebook: isServer && notebookIsATrial(state),
     canUpload: isServer && Boolean(state.notebookInfo.user_can_save)
   };
 }
