7019dd4721fdb7816a205b06c0c0ef40a65a73b6
William Lachance <wlachance@mozilla.com>
2019-08-09 18:27:03 -0400

    Test initialization more precisely
    We can test for the specific redux actions we're looking for, rather
    than just the desired end state. This removes the need to reset
    the redux store between tests.

--- src/editor/initialization/__tests__/handle-initial-iomd.test.js
+++ src/editor/initialization/__tests__/handle-initial-iomd.test.js
@@ -1,38 +1,46 @@
-import { store } from "../../store";
-import { resetNotebook } from "../../actions/actions";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
 import handleInitialIomd from "../handle-initial-iomd";
 
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
 describe("handleInitialIomd is correct when no #iomd elt is found", () => {
-  document.body.innerHTML = `
-<script id="notiomd">foo</script>
-`;
+  let store;
+
   beforeEach(() => {
-    store.dispatch(resetNotebook());
+    document.body.innerHTML = `<script id="notiomd">foo</script>`;
+    store = mockStore({});
   });
 
   it("store is valid when no #iomd elt is found", () => {
     expect(() => handleInitialIomd(store)).not.toThrow();
   });
+
   it("iomd is correct", () => {
     handleInitialIomd(store);
-    expect(store.getState().iomd).toEqual("");
+    expect(store.getActions()).toEqual([]);
   });
 });
 
 describe("handleInitialIomd is correct when #iomd elt is empty", () => {
+  let store;
+
   beforeEach(() => {
-    store.dispatch(resetNotebook());
     document.body.innerHTML = `
 <script id="iomd"></script>
 `;
+    store = mockStore({});
   });
 
   it("store is valid", () => {
     expect(() => handleInitialIomd(store)).not.toThrow();
   });
+
   it("iomd is correct", () => {
     handleInitialIomd(store);
-    expect(store.getState().iomd).toEqual("");
+    expect(store.getActions()).toEqual([]);
   });
 });
 
@@ -41,20 +49,25 @@ describe("handleInitialIomd is correct when #iomd elt has content", () => {
 
 %% js
 blah blah
-    
+
     `;
+  let store;
+
   beforeEach(() => {
-    store.dispatch(resetNotebook());
     document.body.innerHTML = `
 <script id="iomd">${iomdString}</script>
 `;
+    store = mockStore({});
   });
 
   it("store is valid", () => {
     expect(() => handleInitialIomd(store)).not.toThrow();
   });
+
   it("iomd is correct", () => {
     handleInitialIomd(store);
-    expect(store.getState().iomd).toEqual(iomdString);
+    expect(store.getActions()).toEqual([
+      { iomd: iomdString, type: "UPDATE_IOMD_CONTENT" }
+    ]);
   });
 });
