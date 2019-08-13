d9847d4f12197a7283759b59352e5326f4ab7578
hamilton <hamilton.ulmer@gmail.com>
2019-08-13 11:51:18 -0700

    cleaning up the demo

--- src/shared/components/file-drop.jsx
+++ src/shared/components/file-drop.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import styled from "react-emotion";
-import { keyframes } from "emotion";
 
 const FileDropArea = styled.div`
   position: fixed;
@@ -34,7 +33,7 @@ const preventDefaults = event => {
   event.stopPropagation();
 };
 
-const onDragEnter = (setDragStatus, setCandidateFiles) => event => {
+const onDragEnterCreator = (setDragStatus, setCandidateFiles) => event => {
   preventDefaults(event);
   const dt = event.dataTransfer;
   const { files } = dt;
@@ -46,40 +45,63 @@ const onDragOver = event => {
   preventDefaults(event);
 };
 
-const onDragLeave = (setDragStatus, setCandidateFile) => event => {
+const onDragLeaveCreator = (setDragStatus, setCandidateFile) => event => {
   preventDefaults(event);
   setDragStatus(false);
   setCandidateFile(undefined);
 };
 
-const onFileDrop = (onDrop, setCandidateFile, setDragStatus) => event => {
+const onFileDropCreator = (
+  onDrop,
+  setCandidateFile,
+  setDragStatus
+) => event => {
   preventDefaults(event);
   onDrop(event.dataTransfer.files);
   setCandidateFile(undefined);
   setDragStatus(false);
 };
 
+// In this demo, FileDrop has a shrinkTarget, which I used for the animation
+// that shrinks down the window. This is not necessary in the actual
+// implementation, so feel free to ditch it.
 function FileDrop({ shrinkTarget, onDrop }) {
   const [isDragging, setDragStatus] = useState(false);
   const dragElement = useRef(null);
+  // atm candidates and setCandidateFile aren't really used I think, since
+  // I didn't add the interstitial that would show the candidate files
+  // being loaded.
   const [candidates, setCandidateFile] = useState(undefined);
-  const ODE = onDragEnter(setDragStatus, setCandidateFile);
-  const ODL = onDragLeave(setDragStatus, setCandidateFile);
-  const DROP = onFileDrop(onDrop, setCandidateFile, setDragStatus);
+  const onDragEnter = onDragEnterCreator(setDragStatus, setCandidateFile);
+  const onDragLeave = onDragLeaveCreator(setDragStatus, setCandidateFile);
+  const onFileDrop = onFileDropCreator(onDrop, setCandidateFile, setDragStatus);
 
+  // this useEffect runs when the component is initially mounted.
+  // In this particular example, we'll add the event listeners to
+  // window itself instead of a particular element.
+  // The idea is the dragElement pops into place when onDragEnter is fired,
+  // and once it is available, you want to listen for dragleave on
+  // the dragElement (since it takes up the whole window).
+
+  // this is probably substantially easier if you're only allowing the drag and
+  // drop on an individual element vs. the entire window, so I would update this
+  // accordingly.
   useEffect(() => {
-    window.addEventListener("dragenter", ODE, false);
+    window.addEventListener("dragenter", onDragEnter, false);
     window.addEventListener("dragover", onDragOver);
-    dragElement.current.addEventListener("dragleave", ODL, false);
-    window.addEventListener("drop", DROP, false);
+    dragElement.current.addEventListener("dragleave", onDragLeave, false);
+    window.addEventListener("drop", onFileDrop, false);
     return () => {
-      document.body.removeEventListener("dragenter", ODE);
+      document.body.removeEventListener("dragenter", onDragEnter);
       window.removeEventListener("dragover", onDragOver);
-      dragElement.current.removeEventListener("dragleave", ODL, false);
-      dragElement.current.removeEventListener("drop", DROP);
+      dragElement.current.removeEventListener("dragleave", onDragLeave, false);
+      dragElement.current.removeEventListener("drop", onFileDrop);
     };
   }, []);
 
+  // This is used primarily to transition the shrinkTarget.
+  // It is not by any means necessary in the actual version, but here is
+  // how one might attempt to animate something.
   useEffect(() => {
     const transitionTime = 300;
     const half = transitionTime / 2;
@@ -97,7 +119,16 @@ function FileDrop({ shrinkTarget, onDrop }) {
     }
   });
   const str = "explore these files in a notebook";
+  // This may also not be necessary, depending on how you approach
+  // the implementation of the drag and drop functionality. Here,
+  // we actually use ReactDOM.createPortal to inject outside of the
+  // immediate DOM tree another element. This is also how modals
+  // are typically built in React. You'll notice that we set the transform
+  // to 100vw off to the right, and then pop it into place if isDragging.
+  // There are other ways of achieving this effect.
 
+  // And of course, you're free to ignore using ReactDOM.createPortal if you
+  // choose to keep the drag-and-drop over an element area.
   return ReactDOM.createPortal(
     <div
       style={{
