6ace239dbdd1bfffc0e9a69af186531ee171b3b0
bcolloran <bcolloran@mozilla.com>
2019-09-05 15:53:57 -0700

    fix scrollback

--- src/editor/console/input/thunks.js
+++ src/editor/console/input/thunks.js
@@ -4,15 +4,12 @@ import { resetConsole } from "./actions";
 export function evalConsoleInput(consoleText) {
   return (dispatch, getState) => {
     // exit if there is no code in the console to  eval
-    if (!consoleText) {
-      return undefined;
-    }
+    if (!consoleText) return;
     const chunk = {
       chunkContent: consoleText,
       chunkType: getState().languageLastUsed
     };
     dispatch(addToEvalQueue(chunk));
     dispatch(resetConsole());
-    return Promise.resolve();
   };
 }
