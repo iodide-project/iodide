4ea8ac9c492d4b280e4658b24df706da8065ffd3
Jannis Leidel <jannis@leidel.info>
2019-09-10 16:46:43 +0200

    Fix datetime warning.

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -19,6 +19,14 @@ def pytest_configure(config):
 pytest_plugins = ["server.tests.plugins.lean", "server.tests.plugins.staticfiles"]
 
 
+def pytest_configure(config):
+    # work-around for https://github.com/ktosiek/pytest-freezegun/issues/13
+    config.addinivalue_line(
+        "markers",
+        "freeze_time(timestamp): freeze time to the given timestamp for the duration of the test.",
+    )
+
+
 @pytest.fixture
 def api_client():
     """
