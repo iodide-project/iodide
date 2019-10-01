1cc2612e862f2d5fd9a68f03e5aa5c9edea81386
Jannis Leidel <jannis@leidel.info>
2019-10-01 10:38:08 +0200

    Fix migrations.

--- server/files/migrations/0004_capitalized_status_choices.py
+++ server/files/migrations/0004_capitalized_status_choices.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('files', '0002_add_file_sources_and_operations'),
+        ('files', '0003_saner_table_names'),
     ]
 
     operations = [
