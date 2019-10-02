6622f0a2bbcbc871cae4b89610ff7076f10d793c
Jannis Leidel <jannis@leidel.info>
2019-10-02 18:02:05 +0200

    Fix test.

--- server/tests/test_file_update_operations.py
+++ server/tests/test_file_update_operations.py
@@ -142,7 +142,7 @@ def test_post_file_update_operation(fake_user, test_notebook, test_file_source, 
         file_update_operation = FileUpdateOperation.objects.first()
         assert file_update_operation.file_source_id == test_file_source.id
         assert file_update_operation.status == FileUpdateOperation.PENDING
-        assert file_update_operation.scheduled_at.replace(tzinfo=None) == timezone.now()
+        assert file_update_operation.scheduled_at == timezone.now()
         assert file_update_operation.started_at is None
         assert file_update_operation.ended_at is None
 
