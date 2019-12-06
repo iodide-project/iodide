c9136e05fefb8aca83e5ef3214c0419d6f3a9b86
William Lachance <wlachance@mozilla.com>
2019-12-06 12:38:40 -0500

    Unescape hexadecimal quotes in iomd (fixes #2524)
    Django 3.0 escapes quotes in hexadecimal format
    (https://docs.djangoproject.com/en/3.0/ref/utils/#django.utils.html.escape),
    so our unescaping function needs to handle those sequences too.

--- src/editor/initialization/handle-initial-iomd.js
+++ src/editor/initialization/handle-initial-iomd.js
@@ -4,6 +4,8 @@ import { updateIomdContent } from "../actions/editor-actions";
 export default function handleInitialIomd(store) {
   const iomdElt = document.getElementById("iomd");
   if (iomdElt && iomdElt.innerHTML) {
-    store.dispatch(updateIomdContent(_.unescape(iomdElt.innerHTML)));
+    store.dispatch(
+      updateIomdContent(_.unescape(iomdElt.innerHTML).replace(/&#x27;/g, "'"))
+    );
   }
 }
