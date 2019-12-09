9513decb2d247bfbb7a7196d69ba1516291d2ba2
algentile <gogentileorgohome@gmail.com>
2019-12-08 17:53:32 -0800

    remnant for testing lint comment

--- src/editor/index.jsx
+++ src/editor/index.jsx
@@ -40,7 +40,6 @@ import CSSCascadeProvider from "../shared/components/css-cascade-provider";
 import { initializeDefaultKeybindings } from "./initialization/keybindings";
 import { handleInterceptBackspace } from "../shared/intercept-keybindings";
 
-// eslint-disable-next-line func-names
 initializeDefaultKeybindings();
 handleInterceptBackspace();
 
